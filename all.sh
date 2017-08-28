c8a8d9c037579f094f3af0a10d4c1cc371b58bd9
Paul Henning <vxbinaca@gmail.com>
2017-08-28 08:37:29 +0000

    Fixed type in all.sh

--- all.sh
+++ all.sh
@@ -29,5 +29,5 @@ pip3 install -U --user youtube-dl tubeup pip ; ## Update python envionment
 git pull ; ## Refresh from Git repo
 ./inject-merge.sh ; ##Merge new repo changes into local archive
 ./extract.sh ; ## Pull newest video progress
-git pugh origin master ; ## Push to git repo
+git push origin master ; ## Push to git repo
 ./all.sh
