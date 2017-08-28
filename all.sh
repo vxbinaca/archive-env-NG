dc7c09ef6c745b58e0e0bc3ff7a65d247030de40
Paul Henning <vxbinaca@users.noreply.github.com>
2017-08-28 02:06:35 -0400

    Opps forgot the merging part.

--- all.sh
+++ all.sh
@@ -27,6 +27,7 @@
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
 pip3 install -U --user youtube-dl tubeup pip ; ## Update python envionment
 git pull ; ## Refresh from Git repo
+./inject-merge.sh ; ##Merge new repo changes into local archive
 ./extract.sh ; ## Pull newest video progress
 git pugh origin master ; ## Push to git repo
 ./all.sh
