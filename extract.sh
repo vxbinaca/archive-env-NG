683db5a42176fc14e020235ecdafd84290fc75d5
Paul Henning <vxbinaca@gmail.com>
2017-08-28 05:47:17 +0000

    Fixed extraction script

--- extract.sh
+++ extract.sh
@@ -1,6 +1,6 @@
 ##Exfiltrate archive file
-cat ~/.tubeup/.ytdlarchive >> ~/git/archive-progress/.ytdlarchive.temp ; 
-##Mix into temp and then back again
+cat ~/.tubeup/.ytdlarchive >> ~/git/archive-env-NG/.ytdlarchive.temp ; 
+##Mix into temp and then back again, sorting for uniqueness
 cat .ytdlarchive >> .ytdlarchive.temp ; cat .ytdlarchive.temp | sort -u > .ytdlarchive ; yes | rm .ytdlarchive.temp
 ##create commit
 git add .ytdlarchive ; git commit -m "Download archive update"
