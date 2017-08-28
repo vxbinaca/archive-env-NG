4ff2293684fc34f67c825b5e5e526d5ed8657f8c
Paul Henning <vxbinaca@users.noreply.github.com>
2017-08-28 02:48:04 -0400

    Update YTPMV.sh

--- looping/YTPMV.sh
+++ looping/YTPMV.sh
@@ -432,4 +432,5 @@ tubeup --metadata=collection:ytpmv-mad youtube.com/xlethxleth
 tubeup --metadata=collection:ytpmv-mad youtube.com/user/oskari14/videos
 tubeup --metadata=collection:ytpmv-mad youtube.com/channel/UCB0HgUlDmyQJHDbdYI5kZB
 tubeup --metadata=collection:ytpmv-mad youtube.com/channel/UC_8TtUCe4moQYJej25mWuDg
+tubeup --metadata=collection:ytpmv-mad https://www.youtube.com/channel/UCH8QruKx3Qw577yO4U0SmZw
 ./looping/YTPMV.sh
