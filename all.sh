980cde85d337497151d99fe30a51a97f0841d92c
Paul Henning <vxbinaca@gmail.com>
2017-08-29 00:54:00 +0000

    Download archive update

--- all.sh
+++ all.sh
@@ -26,8 +26,8 @@
 ./targets/var.sh ;
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
 pip3 install -U --user youtube-dl tubeup pip ; ## Update python envionment
-git pull ; ## Refresh from Git repo
-./inject-merge.sh ; ##Merge new repo changes into local archive
-./extract.sh ; ## Pull newest video progress
-git push origin master ; ## Push to git repo
-./all.sh
+git pull ; ## Pull new progress from remote server
+./inject-merge.sh ; ## Merge new remote changes into local archive
+./extract.sh ; ## Extract netest entries from local .ytdlarchive
+git push origin master ; ## Push back to github
+./all.sh ## start over again in an infinite loop
--- bigscripts.sh
+++ bigscripts.sh
@@ -4,9 +4,13 @@
 ./looping/periscope.sh ;   
 ./looping/stoners.sh ;
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
-pip3 install -U --user youtube-dl tubeup pip ; ## Update python envionment
-git pull ; ## Refresh from Git repo
-./inject-merge.sh ; ##Merge new repo changes into local archive
-./extract.sh ; ## Pull newest video progress
-git push origin master ; ## Push to git repo
-./bigscripts.sh
+pip3 install -U --user youtube-dl tubeup pip ; ## Update python
+ envionment
+git pull ; ## Pull new progress from remote server
+./inject-merge.sh ; ## Merge new remote changes into local arch
+ive
+./extract.sh ; ## Extract netest entries from local .ytdlarchiv
+e
+git push origin master ; ## Push back to github
+./bigscripts.sh ## start over again in an infinite loop
+
