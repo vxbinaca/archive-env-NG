2d30fb6661d498d868573ec7d2893a96d4af7740
vxbinaca <vxbinaca@protonmail.ch>
2017-09-18 21:51:13 +0000

    Created SFM targets script

---
+++ targets/sfm.sh
@@ -0,0 +1,4 @@
+tubeup https://www.youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ/videos
+tubeup https://www.youtube.com/user/HypotheDragon 
+tubeup https://www.youtube.com/user/COBMetalHead 
+tubeup https://www.youtube.com/user/tf2thewingle
