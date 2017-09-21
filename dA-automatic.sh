84f3ecf16267134d17c906cdd83e40ace26d4716
Paul Henning <vxbinaca@gmail.com>
2017-09-21 15:16:50 -0400

    Fixed output directories

--- dA-automatic.sh
+++ dA-automatic.sh
@@ -1,23 +1,103 @@
-ruby fetch.rb -n https://ajdudebro.deviantart.com/gallery/
-ruby fetch.rb -n https://alexandrab24.deviantart.com/gallery/
-ruby fetch.rb -n https://arrrgyle.deviantart.com/gallery/
-ruby fetch.rb -n https://cyncaelyx.deviantart.com/gallery/
-ruby fetch.rb -n https://digital-snapshots.deviantart.com/gallery/
-ruby fetch.rb -n https://hussard-yr.deviantart.com/gallery/
-ruby fetch.rb -n https://jabbym.deviantart.com/gallery/
-ruby fetch.rb -n https://jessicaelwood.deviantart.com/gallery/
-ruby fetch.rb -n https://jynos.deviantart.com/gallery/
-ruby fetch.rb -n https://kuvshinov-ilya.deviantart.com/gallery/
-ruby fetch.rb -n https://marlomarquise.deviantart.com/gallery/
-ruby fetch.rb -n https://otfs.deviantart.com/gallery/
-ruby fetch.rb -n https://rizzyda.deviantart.com/gallery/
-ruby fetch.rb -n https://salmoneatjo0.deviantart.com/gallery/
-ruby fetch.rb -n https://s-c.deviantart.com/gallery/
-ruby fetch.rb -n https://sourcefilmmaker.deviantart.com/gallery/
-ruby fetch.rb -n https://tainted-kayla.deviantart.com/gallery/
-ruby fetch.rb -n https://thesourkraut.deviantart.com/
-ruby fetch.rb -n https://urbanator.deviantart.com/gallery/
-ruby fetch.rb -n https://xenolsvectors.deviantart.com/gallery/
-ruby fetch.rb -n https://yanhost.deviantart.com/gallery/
-ruby fetch.rb -n https://hakusfm.deviantart.com/
-ruby fetch.rb -n https://splashbrush.deviantart.com/gallery/
+figlet dreamingaboutmonsters
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/dreaming-about-monsters dreaming-about-monsters 
+figlet sacralsex
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/sacralsex sacralsex
+figlet Naked-Yogi
+./tumblr_backup.py --no-reblog --save-video -I o -t me,Me,ME,lover,personal -O /media/vxbinaca/Storage/tumblr-blogs/naked-yogi naked-yogi
+##./tumblr_backup.py --no-reblog --save-video -I o -t me,Me,ME -P prettyplease -O /media/vxbinaca/Storage/tumblr-blogs/pandaslittleone pandaslittleone
+figlet Seattle255
+./tumblr_backup.py --no-reblog --save-video -I o -t ME,SELF,US -O /media/vxbinaca/Storage/tumblr-blogs/seattle255 seattle255
+figlet Penelope
+./tumblr_backup.py --no-reblog --save-video -I o -t penelopemachine -O /media/vxbinaca/Storage/tumblr-blogs/penelopemachine p-e-n-e-l-o-p-e-m-a-c-h-i-n-e
+figlet Jokoss
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/jokoss jokoss
+##./tumblr_backup.py --no-reblog --save-video -I o -t "madame--ette" -O /media/vxbinaca/Storage/tumblr-blogs/"madame--ette" "madame--ette"
+figlet Cam Damage
+./tumblr_backup.py --no-reblog --save-video -I o -t cam-damage,camdamage,camille-damage -O /media/vxbinaca/Storage/tumblr-blogs/camdamage camdamage
+figlet Vera
+./tumblr_backup.py  --no-reblog --save-video -I o -t selfie,fishnets -O /media/vxbinaca/Storage/tumblr-blogs/igetwetwhen igetwetwhen
+figlet Esachasa
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/vinyldolly vinyldolly
+figlet roarie yum
+./tumblr_backup.py --no-reblog --save-video -I o -t roarie -O /media/vxbinaca/Storage/tumblr-blogs/roarieyum roarieyum
+figlet Tankya Dakin
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/tanyadakin tanyadakin
+figlet Rachel
+./tumblr_backup.py --no-reblog --save-video -I o -t porn -O /media/vxbinaca/Storage/tumblr-blogs/rachelgetsravaged rachelgetsravaged 
+figlet esachasa old
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/vinyldollold vinyldollold
+figlet jess elwood
+./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/jsselwood jsselwood
+figlet koreanstudentsspeak
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/koreanstudentsspeak koreanstudentsspeak
+##figlet gethighnfuck
+##./tumblr_backup.py --no-reblog --save-video -I o -t cum,titties,facial,fuck,gethigh-n-fuck,naked,bong,boobies,get-high-and-fuck,self-shot,pregnant -O gethigh-n-fuck gethigh-n-fuck
+figlet sookie-m
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/sookie-m sookie-m
+figlet kayla
+##./tumblr_backup.py --no-reblog --save-video -I o -O taintedkayla taintedkayla
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/lesalpines lesalpines
+figlet foxytail11
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/foxytail11 foxytail11
+figlet extremefoxytail
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/extremefoxytail extremefoxytail
+figlet foxytail11vids
+./tumblr_backup.py --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/foxytail11vids foxytail11vids
+figlet foxytailqanda
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/foxytailqanda foxytailqanda
+figlet Chelsea
+./tumblr_backup.py --no-reblog --save-video -I o -t thebrutalviking -O /media/vxbinaca/Storage/tumblr-blogs/thebrutalviking thebrutalviking
+figlet hakusfm
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/hakusfm hakusfm
+figlet StudioFOW
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/studiofow studiofow
+figlet whitneywisconsin
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/followwhitneywisconsin followwhitneywisconsin
+figlet urbanator123
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/urbanator123 urbanator123
+figlet studioFOW
+#figlet mememolly
+#./tumblr_backup.py --no-reblog --save-video -I o -O mememolly mememolly
+figlet kiefeon
+./tumblr_backup.py --no-reblog --save-video -I o -t me,mine -O /media/vxbinaca/Storage/tumblr-blogs/kiefeon kiefeon
+figlet Gatsbyswhore
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/gatsbyswhore gatsbyswhore
+figlet Aella Girl
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/aellagirl aellagirl
+##figlet eyeball
+##./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/eyeball eyeball
+figlet elmolincoln
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/elmolincoln elmolincoln
+figlet godsinanalcove
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/godsinanalcove godsinanalcove
+figlet wepackedthebowlfortwo
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/wepackedthebowlfortwo wepackedthebowlfortwo
+figlet pandamonster33
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/pandamonster33 pandamonster33
+figlet nice-is-different-than-good
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/nice-is-different-than-good nice-is-different-than-good
+figlet livingshattered
+./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/livingshattered livingshattered
+figlet godshidiouscreation
+./tumblr_backup.py --no-reblog --save-video -I o -t me,art,weed -O /media/vxbinaca/Storage/tumblr-blogs/godshideouscreation godshideouscreation
+figlet msmarvolo 
+./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/msmarvolo  msmarvolo
+figlet littlefae
+./tumblr_backup.py --no-reblog --save-video -I o -t me,mine,kiefon, -O /media/vxbinaca/Storage/tumblr-blogs/thelittlefae thelittlefae
+
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/slippystuff slippystuff
+./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/weedstop weedstop
+./tumblr_backup.py --no-reblog --save-video -I o -t me -O /media/vxbinaca/Storage/tumblr-blogs/daintylungs daintylungs
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/xxxsouthernwifeslut xxxsouthernwifeslut
+./tumblr_backup.py --no-reblog --save-video -I o -t  behindthecamera -O /media/vxbinaca/Storage/tumblr-blogs/eccedentesiastfoxx eccedentesiastfox 
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/hotcumslutcouple hotcumslutcouple
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/h0e-bl0g h0e-bl0g
+./tumblr_backup.py --no-reblog --save-video -I o -t personal,weed,me,myface,myart,mine,1k -O /media/vxbinaca/Storage/tumblr-blogs/stonerjpeg stonerjpeg
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/corgiwitch corgiwitch
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/weed-breath weed-breath
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/babybaked babybaked
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/hash-420 hash-420
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/everystonerhasafuture everystonerhasafuture
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/ali3nchildr3n ali3nchildr3n 
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/paradisiak paradisiak
+./tumblr_backup.py --no-reblog --save-video -I o -O /media/vxbinaca/Storage/tumblr-blogs/purple6rainbow9 purple6rainbow9
