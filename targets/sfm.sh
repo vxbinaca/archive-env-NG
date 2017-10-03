9d71847845e3839ce70aa2c8aa0f8b298c80381b
Paul Henning <vxbinaca@gmail.com>
2017-10-03 04:21:45 -0400

    Fixed targets, eliminated duplicated, sorted list

--- targets/sfm.sh
+++ targets/sfm.sh
@@ -1,4 +1,4 @@
 tubeup https://www.youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ/videos
-tubeup https://www.youtube.com/user/HypotheDragon 
-tubeup https://www.youtube.com/user/COBMetalHead 
-tubeup https://www.youtube.com/user/tf2thewingle
+tubeup https://www.youtube.com/user/HypotheDragon/videos 
+tubeup https://www.youtube.com/user/COBMetalHead/videos
+tubeup https://www.youtube.com/user/tf2thewingle/videos
