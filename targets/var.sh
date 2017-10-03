7fa29ffb6983305981bac03eb4213584d9de4b1d
Paul Henning <vxbinaca@gmail.com>
2017-10-03 04:20:21 -0400

    Fixed targets, eliminated duplicated, sorted list

--- targets/var.sh
+++ targets/var.sh
@@ -1,55 +1,51 @@
-tubeup https://www.youtube.com/channel/UCp8tB7kfhjvmgY2wUDBlaMA
-tubeup http://www.youtube.com/channel/UCp4oMaqaY0hIoT9jafj1g8g
-tubeup https://www.youtube.com/channel/UCKNQ5RbMm42xVVARTJoEsIg
-tubeup https://www.youtube.com/user/shachiNT/about
-tubeup https://www.youtube.com/channel/UC4QFrwuzxJ2eC7Q2iDrlJFg
-tubeup https://www.youtube.com/channel/UCKlA7qF9XKwu79ULYmVu28w
-tubeup https://www.youtube.com/channel/UC1fSp9Qpp8tT982OumLS5Hg
-tubeup http://www.youtube.com/user/RareNargacuga
-tubeup https://www.youtube.com/channel/UCS6F1cpcSoJkztme19_KIzw
-tubeup https://www.youtube.com/channel/UCWH4JkQWioU3vTAritHDnAA
-tubeup https://www.youtube.com/user/LeoPirate/videos
-tubeup https://www.youtube.com/channel/UCWMPmKJ8GmmUvI5ZC0V2VsA
-##NJ nerds
-#tubeup https://www.youtube.com/channel/UCKH9HfYY_GEcyltl2mbD5lA
-#tubeup https://www.youtube.com/user/katTcritic
-#tubeup https://www.youtube.com/user/gotenks6
-#tubeup https://www.youtube.com/user/ejmillennium
-#tubeup https://www.youtube.com/channel/UC_-9rxe8VZI-tUQJWEig4mA
-##
-tubeup https://www.youtube.com/channel/UC8aG3LDTDwNR1UQhSn9uVrw
-tubeup https://www.youtube.com/channel/UCgHUzh9T2DHvSDZcnILqH8g
-tubeup https://www.youtube.com/user/etanthe4th/videos
+#tubeup https://www.youtube.com/channel/UCKH9HfYY_GEcyltl2mbD5lA/videos ##NJ nerds
+#tubeup https://www.youtube.com/channel/UC_-9rxe8VZI-tUQJWEig4mA/videos ##NJ nerds
+#tubeup https://www.youtube.com/channel/UCnbUeHmpeHAX3xED4fRXKeg/videos ## Vaporwave
+#tubeup https://www.youtube.com/user/ejmillennium/videos ##NJ nerds
+#tubeup https://www.youtube.com/user/gotenks6/videos ##NJ nerds
+#tubeup https://www.youtube.com/user/katTcritic/videos ##NJ nerds
+tubeup http://www.youtube.com/channel/UCp4oMaqaY0hIoT9jafj1g8g/videos
+tubeup http://www.youtube.com/user/RareNargacuga/videos
+tubeup https://www.youtube.com/channel/UC1fSp9Qpp8tT982OumLS5Hg/videos
+tubeup https://www.youtube.com/channel/UC4QFrwuzxJ2eC7Q2iDrlJFg/videos
+tubeup https://www.youtube.com/channel/UC5hie5xBr7qENWVD_kfsiew/videos
+tubeup https://www.youtube.com/channel/UC7yPuRdd4ZKhJQueiVnjd2A/videos
+tubeup https://www.youtube.com/channel/UC8EvY5Cky6LM9tTE07kk2kQ/video ## Vaporwave
+tubeup https://www.youtube.com/channel/UC8aG3LDTDwNR1UQhSn9uVrw/videos
+tubeup https://www.youtube.com/channel/UCAo6nC6I9yk2X4xqQThhhew/videos
 tubeup https://www.youtube.com/channel/UCEAqHbeIpPERhniNjQyR7EQ/videos
-## Vaporwave
-tubeup https://www.youtube.com/channel/UC8EvY5Cky6LM9tTE07kk2kQ
-#tubeup https://www.youtube.com/channel/UCnbUeHmpeHAX3xED4fRXKeg
-##
+tubeup https://www.youtube.com/channel/UCEUrQzNPvDQoEMNswAr8mCA/videos
+tubeup https://www.youtube.com/channel/UCGCqGI3f2WSDnfRjrD1Cj_Q/videos
+tubeup https://www.youtube.com/channel/UCKNQ5RbMm42xVVARTJoEsIg/videos
+tubeup https://www.youtube.com/channel/UCKlA7qF9XKwu79ULYmVu28w/videos
+tubeup https://www.youtube.com/channel/UCNEo4Lb14UTkcqBZ9nKwyTg/videos
+tubeup https://www.youtube.com/channel/UCS6F1cpcSoJkztme19_KIzw/videos
+tubeup https://www.youtube.com/channel/UCT5y7BAocgM-PeXpVCvyG4g/videos
+tubeup https://www.youtube.com/channel/UCTlQFIx8RUus4R3zYAUHHYA/videos
+tubeup https://www.youtube.com/channel/UCWH4JkQWioU3vTAritHDnAA/videos
+tubeup https://www.youtube.com/channel/UCWMPmKJ8GmmUvI5ZC0V2VsA/videos
+tubeup https://www.youtube.com/channel/UCZZFrPKyH-Le2uoPGnn5jyg/videos
+tubeup https://www.youtube.com/channel/UCdpkbiiCFHVPZrSYEEWyDdw/videos
 tubeup https://www.youtube.com/channel/UCe6CpThL4G2RRHfaIv0b-1w/videos
-tubeup https://www.youtube.com/user/ProfessorFennec/videos
-tubeup https://www.youtube.com/channel/UCGCqGI3f2WSDnfRjrD1Cj_Q
-tubeup https://www.youtube.com/channel/UCEUrQzNPvDQoEMNswAr8mCA
-tubeup https://www.youtube.com/channel/UCZZFrPKyH-Le2uoPGnn5jyg
-tubeup https://www.youtube.com/user/littleloca/videos
-tubeup https://www.youtube.com/user/TheRealParis/videos
-tubeup https://www.youtube.com/user/Ooolalaa/videos
-tubeup https://www.youtube.com/user/stevieryan/videos
-tubeup https://www.youtube.com/channel/UCkfqhn4lyofss8PM1IubDgA
+tubeup https://www.youtube.com/channel/UCecsVoeJcsXbAra7Sl4mOPw/videos
+tubeup https://www.youtube.com/channel/UCgHUzh9T2DHvSDZcnILqH8g/videos
+tubeup https://www.youtube.com/channel/UCkDXV1ELQBVV4BuLR-FyfkA/videos
+tubeup https://www.youtube.com/channel/UCkfqhn4lyofss8PM1IubDgA/videos
+tubeup https://www.youtube.com/channel/UCp8tB7kfhjvmgY2wUDBlaMA/videos
+tubeup https://www.youtube.com/channel/UCuimpDKCmUaW6qnMMp7XE7Q/videos
 tubeup https://www.youtube.com/user/0dutch/videos
-tubeup https://www.youtube.com/channel/UCTlQFIx8RUus4R3zYAUHHYA 
+tubeup https://www.youtube.com/user/BraveTheWorldChannel/videos
+tubeup https://www.youtube.com/user/LeoPirate/videos
+tubeup https://www.youtube.com/user/Ooolalaa/videos
+tubeup https://www.youtube.com/user/ProfessorFennec/videos
+tubeup https://www.youtube.com/user/TheRealParis/videos
+tubeup https://www.youtube.com/user/debuglive/videos
+tubeup https://www.youtube.com/user/dsscdse/videos
+tubeup https://www.youtube.com/user/etanthe4th/videos
 tubeup https://www.youtube.com/user/flippingpages1014/videos
-tubeup https://www.youtube.com/user/debuglive
-tubeup https://www.youtube.com/user/lonelygirl15
-tubeup https://www.youtube.com/channel/UC5hie5xBr7qENWVD_kfsiew
-tubeup https://www.youtube.com/channel/UC7yPuRdd4ZKhJQueiVnjd2A
-tubeup https://www.youtube.com/channel/UCNEo4Lb14UTkcqBZ9nKwyTg
-tubeup https://www.youtube.com/channel/UCdpkbiiCFHVPZrSYEEWyDdw
-tubeup https://www.youtube.com/channel/UCT5y7BAocgM-PeXpVCvyG4g
-tubeup https://www.youtube.com/user/dsscdse/
-tubeup https://www.youtube.com/channel/UCuimpDKCmUaW6qnMMp7XE7Q
-tuneup https://www.youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew
+tubeup https://www.youtube.com/user/littleloca/videos
+tubeup https://www.youtube.com/user/lonelygirl15/videos
+tubeup https://www.youtube.com/user/shachiNT/videos
+tubeup https://www.youtube.com/user/stevieryan/videos
 tubeup https://www.youtube.com/watch?v=xTJC2RkcLEs&list=PLD0NeEbRY7VTkw1GBrnsZAUIrOTOjyzaw
-tubeup https://www.youtube.com/channel/UCkDXV1ELQBVV4BuLR-FyfkA
-tubeup https://www.youtube.com/user/BraveTheWorldChannel
-tubeup https://www.youtube.com/channel/UCAo6nC6I9yk2X4xqQThhhew
-tubeup https://www.youtube.com/channel/UCecsVoeJcsXbAra7Sl4mOPw
+tuneup https://www.youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew/videos
