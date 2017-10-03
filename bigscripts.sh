f550a33a398be509f4155a55662110faaa2d2750
Paul Henning <vxbinaca@gmail.com>
2017-10-03 14:15:59 -0400

    Fixed text

--- bigscripts.sh
+++ bigscripts.sh
@@ -4,13 +4,10 @@
 ./targets/periscope.sh ;   
 ./targets/stoners.sh ;
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
-pip3 install -U --user youtube-dl tubeup pip ; ## Update python
- envionment
+pip3 install -U --user youtube-dl tubeup pip ; ## Update python envionment
 git pull ; ## Pull new progress from remote server
-./inject-merge.sh ; ## Merge new remote changes into local arch
-ive
-./extract.sh ; ## Extract netest entries from local .ytdlarchiv
-e
+./inject-merge.sh ; ## Merge new remote changes into local archive
+./extract.sh ; ## Extract netest entries from local .ytdlarchive
 git push origin master ; ## Push back to github
 ./bigscripts.sh ## start over again in an infinite loop
 
