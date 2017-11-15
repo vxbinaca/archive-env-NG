b09c9961e45abb48db7e24d7a33f470ef57b3301
Paul Henning <vxbinaca@protonmail.ch>
2017-11-15 10:18:48 -0500

    Added Wanimal

--- targets/var.sh
+++ targets/var.sh
@@ -51,3 +51,4 @@ tubeup https://www.youtube.com/watch?v=xTJC2RkcLEs&list=PLD0NeEbRY7VTkw1GBrnsZAU
 tubeup https://www.youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew/videos
 tubeup https://www.youtube.com/channel/UCECs2QPEUC-3fNsDYJM1BjQ/videos
 tubeup https://vimeo.com/junnnktank
+tubeup https://vimeo.com/user26842840
