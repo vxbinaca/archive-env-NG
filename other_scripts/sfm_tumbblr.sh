4e8ce1c01545ccd0a8f8047e63073f9659b81d12
Paul Henning <vxbinaca@protonmail.ch>
2017-12-24 19:58:34 -0500

    Created SFM Tumblr script

---
+++ other_scripts/sfm_tumbblr.sh
@@ -0,0 +1,11 @@
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/pallidsfm pallidsfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/zalivstok zalivstok
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/scoutssfm scoutssfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/kushishekku kushishekku
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/notlesdiassfm notlesdiassfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/bayernsfm bayernsfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/gwampsfm gwampsfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/mrsmugbastard mrsmugbastard
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/hakusfm hakusfm
+./tumblr_backup.py --save-video -i -I o -O /media/vxbinaca/Storage/tumblr-blogs/studiofow studiofow
+
