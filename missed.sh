9bcf4d9092ba611955f08efc5b40eb304b63f681
Paul Henning <vxbinaca@protonmail.ch>
2018-01-07 19:02:11 -0500

    Fixed bad filename

--- missed.sh
+++ missed.sh
@@ -1,5 +1,5 @@
 ls ~/.tubeup/downloads/*.mp4 >> ~/git/archive-env-NG/missed_videos.txt # Append MP4 files to list
 ls ~/.tubeup/downloads/*.mkv >> ~/git/archive-env-NG/missed_videos.txt ## Append MKV files to list
-cat missed_files.txt | sort -u > missed2.txt ; cat missed2.txt > missed_files.txt ; rm missed2.txt 
+cat missed_videos.txt | sort -u > missed2.txt ; cat missed2.txt > missed_videos.txt ; rm missed2.txt 
 git add missed_files.sh ; git commit -m "Updated missed uploads"
 
