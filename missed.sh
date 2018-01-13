85329910db2cc3b1f4ed2b8a769bef93118e0c15
Paul Henning <vxbinaca@protonmail.ch>
2018-01-13 18:33:34 -0500

    Okay fixed a lot of mistakes and changed things

--- missed.sh
+++ missed.sh
@@ -1,5 +1,7 @@
-ls ~/.tubeup/downloads/*.mp4 >> ~/git/archive-env-NG/.missed # Append MP4 files to list
-ls ~/.tubeup/downloads/*.mkv >> ~/git/archive-env-NG/.missed ## Append MKV files to list
+cd ~/.tubeup/downloads/
+ls *.mp4 >> ~/git/archive-env-NG/.missed ## Append MP4 files to list
+ls *.mkv >> ~/git/archive-env-NG/.missed ## Append MKV files to list
+cd ~/git/archive-env-NG/
 cat .missed | sort -u > .missed2 ; cat .missed2 > .missed ; rm .missed2 
 git add .missed ; git commit -m "Updated leftover failed uploads"
 
