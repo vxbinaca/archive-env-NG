e71c4a66f18c7eb1348ad4a2594d2cb93f375eab
Paul Henning <vxbinaca@protonmail.ch>
2018-02-27 13:49:38 -0500

    Added jap gun channel

--- targets/var-jap.sh
+++ targets/var-jap.sh
@@ -3,3 +3,4 @@ tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.co
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/pikopikoken/videos
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/fuzzyfunction/videos
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCE4yx7b0-zCtj5QmbFUCvHg
+tubeup --use-download-archive --metadata=collection:tubeup https://www.youtube.com/channel/UCDLA7Y_6mt-9UrrxTuudt8Q
