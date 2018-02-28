8c42d0525c56924cf1b0d722c8fcd30a03504fe1
Your Name <vxbinaca@protonmail.ch>
2018-02-28 00:28:50 +0000

    Combined scripts to lessen server stress and retard Twitch ingestion

--- all.sh
+++ all.sh
@@ -24,11 +24,23 @@
 ./targets/batfe.sh
 ./targets/var-jap.sh ;
 ./targets/var.sh ;
+
+##################################
+####### Big scripts below ########
+##################################
+
+./targets/doller.sh ;
+./targets/speedpainting.sh ;
+./targets/YTPMV.sh ;
+#./targets/periscope.sh ;
+./targets/stoners.sh ;
+#./targets/social-media.sh ; ##disabled for now until problems can be debugged
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
-pip3 install -U --user youtube-dl pip internetarchive; ## Update python envionment
+pip3 install -U --user youtube-dl pip internetarchive; ## Update python envionm
+ent
 git pull ; ## Pull new progress from remote server
 ./inject-merge.sh ; ## Merge new remote changes into local archive
-./extract.sh ; ## Extract newest entries from local .ytdlarchive
+./extract.sh ; ## Extract netest entries from local .ytdlarchive
 #./missed.sh ## Track missed uploads
 git push origin master ; ## Push back to github
-./all.sh ## start over again in an infinite loop
+./bigscripts.sh ## start over again in an infinite loo
