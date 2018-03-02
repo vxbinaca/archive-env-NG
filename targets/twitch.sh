da98547b8be4c7dc9956c1ae9c93eaf1f33843f3
Your Name <vxbinaca@protonmail.ch>
2018-03-02 08:30:17 +0000

    Added ZONEsama to Twitch ingestion

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -3,3 +3,4 @@ tubeup --use-download-archive http://www.twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://www.twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://www.twitch.tv/sydsnap/videos/all
 tubeup --use-download-archive http://www.twitch.tv/shadbasemurdertv/videos/all
+tubeup --use-download-archive http://www.twitch.tv/ZONEsama/videos/all
