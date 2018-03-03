4041415b8237fe683148b954b359cbc06e958117
Your Name <vxbinaca@protonmail.ch>
2018-03-03 06:54:47 +0000

    Added ZONEsama to Twitch ingestion

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -1,6 +1,6 @@
-tubeup --use-download-archive http://www.twitch.tv/afoxinspaceshow/videos/all
-tubeup --use-download-archive http://www.twitch.tv/kyleeasakura/videos/all
-tubeup --use-download-archive http://www.twitch.tv/gamecatt/videos/all
-tubeup --use-download-archive http://www.twitch.tv/sydsnap/videos/all
-tubeup --use-download-archive http://www.twitch.tv/shadbasemurdertv/videos/all
-tubeup --use-download-archive http://www.twitch.tv/ZONEsama/videos/all
+tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
+tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
+tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
+tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
+tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
+tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
