34414cf0a8d7ab97408c1d9a49f1458b909f18c4
Paul Henning <vxbinaca@protonmail.ch>
2018-03-04 16:32:15 -0500

    Added Ava Lenore

--- targets/var.sh
+++ targets/var.sh
@@ -55,3 +55,4 @@ tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.co
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCjPao-PMHoC1IBurivhfGFQ/videos
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCnzq0TEm7vMLUe4wEumA39A
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCpMMcYVSnqOIJA11vBnFngQ/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCKcibNqpgyXwInQHS4s_I4Q/videos
