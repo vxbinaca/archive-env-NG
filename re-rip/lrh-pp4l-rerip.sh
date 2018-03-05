a236c78c00c7a1f2fe2638b93ea9caad56a7c14a
vxbinaca <vxbinaca@protonmail.ch>
2018-03-05 05:14:47 -0500

    Trimming

--- re-rip/lrh-pp4l-rerip.sh
+++
@@ -1,4 +0,0 @@
-youtube-dl --ignore-config --get-id http://www.youtube.com/user/r0pav/videos >> LRH-IDS.txt
-youtube-dl --ignore-config --get-id http://www.youtube.com/user/l0de >> LRH-IDS.txt
-youtube-dl --ignore-config --get-id http://www.youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ >> LRH-IDS.txt
-youtube-dl --ignore-config --get-id http://www.youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng >> LRH-IDS.txt
