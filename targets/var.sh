9f784949545bc18ea0990c715745ea37fb85e7f9
vxbinaca <vxbinaca@protonmail.ch>
2018-03-07 02:06:48 -0500

    Added topless Russian DJ channel

--- targets/var.sh
+++ targets/var.sh
@@ -54,3 +54,4 @@ tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.co
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCnzq0TEm7vMLUe4wEumA39A
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCpMMcYVSnqOIJA11vBnFngQ/videos
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCKcibNqpgyXwInQHS4s_I4Q/videos
+tubeup --use-download-archive --metadata=collection:tubeup https://www.youtube.com/channel/UCTRpJwRhE-uifL7fTRVe48g
