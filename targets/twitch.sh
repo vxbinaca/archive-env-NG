39061b8347f6082ce1f5d7de515a97142ea8b243
vxbinaca <vxbinaca@protonmail.ch>
2018-03-11 04:14:30 -0400

    Added targets, sorted

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -1,6 +1,7 @@
 tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
-tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
-tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
+tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
+tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
+tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
 tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
