f503863e38c3a9224b259a291760a635df2c42a2
vxbinaca <vxbinaca@protonmail.ch>
2018-03-31 04:13:30 -0400

    Update

---
+++ re-rip/vimeo-rip.sh
@@ -0,0 +1,9 @@
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/fetishfactory > vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/toplesstopics >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/sukaoff >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/exey >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/user26842840 >> vimeo-IDs.sh
+#youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/junnnktank >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/user1944739 >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/user24127837 >> vimeo-IDs.sh
+youtube-dl --ignore-config --get-id --ignore-error http://vimeo.com/kuvshinovilya >> vimeo-IDs.sh
