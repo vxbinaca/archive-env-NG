e0adcbec9173533deb8c6be41a798a78d647e00b
Your Name <vxbinaca@protonmail.ch>
2018-04-28 23:19:44 +0000

    Added target, sorted

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -5,4 +5,5 @@ tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
 tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
+tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
 ## tubeup --use-download-archive http://twitch.tv/brittanyventi/videos/all
