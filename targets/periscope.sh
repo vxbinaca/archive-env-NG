3fc640d2ed046f2708f4556d156aa06faeaa73fe
vxbinaca <vxbinaca@protonmail.ch>
2018-05-02 02:22:24 -0400

    Added target, sorted

--- targets/periscope.sh
+++ targets/periscope.sh
@@ -1,104 +1,107 @@
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/2high4lifeTv/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/3tokes/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/6Chel6sea6/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/AlondraBravoo/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/ArtsAndKush/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/Blazingit365/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/CannaYaas/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/CoralReefer420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/CoureyPie/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/Dabbing_Granny/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/DakotaPaige/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/DakotaPaige420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/DandyasLionz
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/HannahWeedWorld
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/HighFromPiper/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/IAmStella420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/JoJosmiles/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/KushNKurls/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/Lilmissmj
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/LindaDMuzzey/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/Macdizzle420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/MarMarkila/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/ModestMaryJane/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/NessiesKitches/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/PositiveSmash/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/Rachellovesyou/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/RavenclawReefer/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/RawOG420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/RemedyTHC_tv/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/SheSmokesJoints/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/SilencedHippie/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/SkullKid_/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/SlowerFuture
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/StoneySpike/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/TheDabSpot/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/TwitchyBlaze/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/TwoStonesGirls/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/WhynterP
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/_kassidymae/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/acidsouul
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/amandalacyxo/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/batgirlbrit/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/bby_serena/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/brigittah91/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/dabmom420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/dabpwincess/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/dsativa/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/dudeletsflyaway/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/earlymorningbonghits/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/earth2julia/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/gingerkidlife/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/godshideouscreation/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/haley420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/highfelisha/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/hiiighhprincessss/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/honeynuggs
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/jaylikejordans/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/joyyyyag/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/juliadabs/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/kid4nothing/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/ladyabigale/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/longlivetheweeds
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/marylovesglass/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/misspeeksa
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/moneyygame/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/mskushton420/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/paperseatbelttt/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/richie_v09/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/royalcannabis/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/sofaseahorses/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/sweetsunriseX
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/theHazelverse/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/thedanksideofthemoon/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/thehashcorner/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/thelittlefae/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/themisslw/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/therealemeliee/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/theweedqueen/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/vnoriegaaa/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/weedbreath/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/weedgoddess/
-tubeup --use-download-archive  --metadata=collection:weedtubers http://www.periscope.tv/rachelhiiiii
-tubeup --use-download-archive  http://www.periscope.tv/AngelMarie/
-tubeup --use-download-archive  http://www.periscope.tv/AverageSwingers/
-tubeup --use-download-archive  http://www.periscope.tv/FetishDollEmily/
-tubeup --use-download-archive  http://www.periscope.tv/FunLG/
-tubeup --use-download-archive  http://www.periscope.tv/KaashButter/
-tubeup --use-download-archive  http://www.periscope.tv/KinoYoga/
-tubeup --use-download-archive  http://www.periscope.tv/LanieLynnXXX
-tubeup --use-download-archive  http://www.periscope.tv/StephyScopes/
-tubeup --use-download-archive  http://www.periscope.tv/SwingerMexico_/
-tubeup --use-download-archive  http://www.periscope.tv/YoungLiving87/
-tubeup --use-download-archive  http://www.periscope.tv/YourOasisRadio/
-tubeup --use-download-archive  http://www.periscope.tv/arabellamascari
-tubeup --use-download-archive  http://www.periscope.tv/ariesqueen_/
-tubeup --use-download-archive  http://www.periscope.tv/dankkcoffee/
-tubeup --use-download-archive  http://www.periscope.tv/harper_cox_
-tubeup --use-download-archive  http://www.periscope.tv/kaylorsaurus
-tubeup --use-download-archive  http://www.periscope.tv/konmann702/
-tubeup --use-download-archive  http://www.periscope.tv/ready2jam1/
-tubeup --use-download-archive  http://www.periscope.tv/sammisacavage/
-tubeup --use-download-archive  http://www.periscope.tv/sheslulu/
-tubeup --use-download-archive  http://www.periscope.tv/veradevera/
-tubeup --use-download-archive  http://www.pscp.tv/thereallisaann/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/2high4lifeTv/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/3tokes/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/6Chel6sea6/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/AlondraBravoo/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/ArtsAndKush/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/Blazingit365/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/CannaYaas/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/CoralReefer420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/CoureyPie/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/Dabbing_Granny/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/DakotaPaige/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/DakotaPaige420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/DandyasLionz
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/HannahWeedWorld
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/HighFromPiper/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/IAmStella420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/JoJosmiles/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/KushNKurls/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/Lilmissmj
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/LindaDMuzzey/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/Macdizzle420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/MarMarkila/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/ModestMaryJane/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/NessiesKitches/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/PositiveSmash/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/Rachellovesyou/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/RavenclawReefer/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/RawOG420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/RemedyTHC_tv/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/SheSmokesJoints/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/SilencedHippie/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/SkullKid_/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/SlowerFuture
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/StoneySpike/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/TheDabSpot/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/TwitchyBlaze/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/TwoStonesGirls/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/WhynterP
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/_kassidymae/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/acidsouul
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/amandalacyxo/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/batgirlbrit/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/bby_serena/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/brigittah91/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/dabmom420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/dabpwincess/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/dsativa/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/dudeletsflyaway/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/earlymorningbonghits/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/earth2julia/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/gingerkidlife/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/godshideouscreation/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/haley420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/highfelisha/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/hiiighhprincessss/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/honeynuggs
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/jaylikejordans/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/joyyyyag/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/juliadabs/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/kid4nothing/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/ladyabigale/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/longlivetheweeds
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/marylovesglass/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/misspeeksa
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/moneyygame/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/mskushton420/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/paperseatbelttt/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/richie_v09/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/royalcannabis/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/sofaseahorses/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/sweetsunriseX
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/theHazelverse/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/thedanksideofthemoon/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/thehashcorner/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/thelittlefae/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/themisslw/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/therealemeliee/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/theweedqueen/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/vnoriegaaa/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/weedbreath/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/weedgoddess/
+tubeup --use-download-archive --metadata=collection:weedtubers http://www.periscope.tv/rachelhiiiii
+tubeup --use-download-archive http://www.periscope.tv/AngelMarie/
+tubeup --use-download-archive http://www.periscope.tv/AverageSwingers/
+tubeup --use-download-archive http://www.periscope.tv/FetishDollEmily/
+tubeup --use-download-archive http://www.periscope.tv/FunLG/
+tubeup --use-download-archive http://www.periscope.tv/KaashButter/
+tubeup --use-download-archive http://www.periscope.tv/KinoYoga/
+tubeup --use-download-archive http://www.periscope.tv/LanieLynnXXX
+tubeup --use-download-archive http://www.periscope.tv/StephyScopes/
+tubeup --use-download-archive http://www.periscope.tv/SwingerMexico_/
+tubeup --use-download-archive http://www.periscope.tv/YoungLiving87/
+tubeup --use-download-archive http://www.periscope.tv/YourOasisRadio/
+tubeup --use-download-archive http://www.periscope.tv/arabellamascari
+tubeup --use-download-archive http://www.periscope.tv/ariesqueen_/
+tubeup --use-download-archive http://www.periscope.tv/dankkcoffee/
+tubeup --use-download-archive http://www.periscope.tv/harper_cox_
+tubeup --use-download-archive http://www.periscope.tv/kaylorsaurus
+tubeup --use-download-archive http://www.periscope.tv/konmann702/
+tubeup --use-download-archive http://www.periscope.tv/ready2jam1/
+tubeup --use-download-archive http://www.periscope.tv/sammisacavage/
+tubeup --use-download-archive http://www.periscope.tv/sheslulu/
+tubeup --use-download-archive http://www.periscope.tv/veradevera/
+tubeup --use-download-archive http://www.pscp.tv/thereallisaann/
+tubeup --use-download-archive http://www.pscp.tv/imutimes/
+tubeup --use-download-archive http://www.pscp.tv/allycat77/
+tubeup --use-download-archive http://www.pscp.tv/anireal/
