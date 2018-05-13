bfad01d71520b55f242c401e1874adc1d231dc68
Your Name <vxbinaca@protonmail.ch>
2018-05-13 10:02:57 +0000

    Added target, sorted

--- targets/vimeo.sh
+++ targets/vimeo.sh
@@ -1,11 +1,10 @@
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/fetishfactory
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/toplesstopics
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/sukaoff
+##tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/toplesstopics
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/exey
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user26842840
+tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/fetishfactory
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/junnnktank
+tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/kuvshinovilya
+tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/sukaoff
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user1944739
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user24127837
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/kuvshinovilya
-
-
+tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user26842840
+tubeup --use-download-archive --metadata=collection:weedtubers http://vimeo.com/TheDabSpot
