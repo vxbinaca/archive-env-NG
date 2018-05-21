a175a1b3aeb6bda366fa0fb40db367749c1ea9f8
Paul Henning <vxbinaca@protonmail.ch>
2018-05-21 07:12:07 -0400

    Re-enabled Charles Carrol Twitch ingestion

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -1,11 +1,11 @@
+## tubeup --use-download-archive http://twitch.tv/brittanyventi/videos/all
+tubeup --use-download-archive http://twitch.tv/SashaHippie/videos/all
 tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
+tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
 tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
-tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
 tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
-tubeup --use-download-archive http://twitch.tv/SashaHippie/videos/all
-## tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
-## tubeup --use-download-archive http://twitch.tv/brittanyventi/videos/all
+tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
