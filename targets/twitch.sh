b647dfff90530aad3a144e5bcbb438d272d98048
Paul Henning <vxbinaca@protonmail.ch>
2018-05-27 10:35:08 -0400

    Added Kayla Brady twitch ingestion

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -4,6 +4,7 @@ tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
 tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
+tubeup --use-download-archive http://twitch.tv/kimimoons/videos/all
 tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
