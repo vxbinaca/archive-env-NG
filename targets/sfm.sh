48641a0a0b4dbfdcfe623ce4aaed64c1b0291f57
Paul Henning <vxbinaca@protonmail.ch>
2018-05-27 01:23:29 -0400

    Added StudioFOW

--- targets/sfm.sh
+++ targets/sfm.sh
@@ -3,3 +3,4 @@ tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.co
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/COBMetalHead
 tubeup --use-download-archive --metadata=collection:tubeup https://www.youtube.com/user/tf2thewinglet
 tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew
