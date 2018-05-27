883b0ecaf35553dc722d3180aa92306ef8d0c1f6
vxbinaca <vxbinaca@protonmail.ch>
2018-05-27 09:06:10 -0400

    Added Jason Scott twitch account

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -7,5 +7,6 @@ tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
 tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
+tubeup --use-download-archive http://twitch.tv/textfilesdotcom/videos/all
 tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
 tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
