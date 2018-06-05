4859b2cd920b766639ee8912da8f0599f55adc57
Your Name <vxbinaca@protonmail.ch>
2018-06-05 08:46:20 +0000

    Commented out kristiplays channel

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -5,7 +5,7 @@ tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
 tubeup --use-download-archive http://twitch.tv/kimimoons/videos/all
-tubeup --use-download-archive http://twitch.tv/kristiplays/videos/all
+## tubeup --use-download-archive http://twitch.tv/kristiplays/videos/all
 tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
