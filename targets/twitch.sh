b7aa676632c47ecc2a94df11bc90d0c141c8982f
Paul Henning <vxbinaca@protonmail.ch>
2018-06-11 07:10:13 -0400

    Uncommented KristiPlays, added Noah Antiweiler

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -5,9 +5,10 @@ tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
 tubeup --use-download-archive http://twitch.tv/kimimoons/videos/all
-## tubeup --use-download-archive http://twitch.tv/kristiplays/videos/all
+tubeup --use-download-archive http://twitch.tv/kristiplays/videos/all
 tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
 tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
+tubeup --use-download-archive http://twitch.tv/spoonyone/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
 tubeup --use-download-archive http://twitch.tv/textfilesdotcom/videos/all
 tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
