d702415dd53733037be5ce0084e876404616a349
Paul Henning <vxbinaca@protonmail.ch>
2018-06-16 21:29:02 -0400

    Added targets

--- targets/gunz.sh
+++ targets/gunz.sh
@@ -1,7 +1,10 @@
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC8q6FZliLdeiMbyFVgOdgfw
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC9k2WzcJ1kUMl4KIwNQwrFA
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCCdeHKahyMAlPV6SQGx8psg
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCClIfWXtn4rA-1jDHAVPYmg
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCW0V8XgHkmT5nsWdw1Z0LFw
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCci79_hMFw1CahtThZJ53yA
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCduPsSVIdaVTRSbCa3Dz78g
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCi5PGj0TV3wkwKmRACb5tHA
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCiqYKuIJRIr2PEF2FOLFOew
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCwFQw-t3fzE6tdd6-KeDMfA
@@ -21,4 +24,3 @@ tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/us
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/royalnonesuchshow
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/saunixcomp
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/tnoutdoors9
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC9k2WzcJ1kUMl4KIwNQwrFA
