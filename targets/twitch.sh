c21ddc38d0407383b65b184e0811aedcfc0b7b78
Your Name <vxbinaca@protonmail.ch>
2018-06-26 05:51:59 +0000

    Added Chubby Kitten Reviews

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -2,6 +2,7 @@
 tubeup --use-download-archive http://twitch.tv/SashaHippie/videos/all
 tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
 tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
+tubeup --use-download-archive http://twitch.tv/chubbykittenreviews/all
 tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
 tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
 tubeup --use-download-archive http://twitch.tv/hydeart/videos/all
@@ -14,6 +15,6 @@ tubeup --use-download-archive http://twitch.tv/spoonyone/videos/all
 tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
 tubeup --use-download-archive http://twitch.tv/textfilesdotcom/videos/all
 tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
+tubeup --use-download-archive http://twitch.tv/yamatoworks/videos/all
 tubeup --use-download-archive http://twitch.tv/zeldacw/videos/all
 tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
-tubeup --use-download-archive http://twitch.tv/yamatoworks/videos/all
