28bfea0adb848f44c9204e3c74fe7f5731105e57
Your Name <vxbinaca@protonmail.ch>
2018-07-05 06:45:51 +0000

    Wording and typo corrections

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 youtube-dl --rm-cache-dir ; ## Cleanse youtube-dl cache
 pip3 install -U --user youtube-dl pip internetarchive; ## Update python envionment
-git pull ; ## Pull new progress from remote server
-./inject-merge.sh ; ## Merge new remote changes into local archive
-./extract.sh ; ## Extract netest entries from local .ytdlarchive
-crontab -l > crontab.lst ; git add crontab.lst ; git commit -m "Updated Crontabs" ## Sync crontab changes with git
+git pull ; ## Pull new progress from repo
+./inject-merge.sh ; ## Merge repo into local instance
+./extract.sh ; ## Extract newest entries from local .ytdlarchive
+crontab -l > crontab.lst ; git add crontab.lst ; git commit -m "Updated Crontabs" ## Sync crontab changes with repo
 ./missed.sh ## Track missed uploads
-git push origin master ; ## Push back to github
+git push origin master ; ## Push back to repo
