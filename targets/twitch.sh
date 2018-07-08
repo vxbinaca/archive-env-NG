4592d2b3931b3d73ba1b363c3dbae857c4bf2c8b
Your Name <vxbinaca@protonmail.ch>
2018-07-08 04:14:56 +0000

    Set collection for all targets

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -1,23 +1,23 @@
-## DEAD CHANNEL ## tubeup --use-download-archive http://twitch.tv/brittanyventi/videos/all
-tubeup --use-download-archive  --metadata=collection:healthyaddictvideos http://twitch.tv/fakegamergirl/videos/all 
-tubeup --use-download-archive http://twitch.tv/SashaHippie/videos/all
-tubeup --use-download-archive http://twitch.tv/afoxinspaceshow/videos/all
-tubeup --use-download-archive http://twitch.tv/charlscarroll/videos/all
-tubeup --use-download-archive http://twitch.tv/chubbykittenreviews/videos/all
-tubeup --use-download-archive http://twitch.tv/gamecatt/videos/all
-tubeup --use-download-archive http://twitch.tv/gunsarefun911/videos/all
-tubeup --use-download-archive http://twitch.tv/hydeart/videos/all
-tubeup --use-download-archive http://twitch.tv/jellykin/videos/all
-tubeup --use-download-archive http://twitch.tv/kimimoons/videos/all
-tubeup --use-download-archive http://twitch.tv/kristiplays/videos/all
-tubeup --use-download-archive http://twitch.tv/kyleeasakura/videos/all
-tubeup --use-download-archive http://twitch.tv/ryuudog_/videos/all
-tubeup --use-download-archive http://twitch.tv/shadbasemurdertv/videos/all
-tubeup --use-download-archive http://twitch.tv/spoonyone/videos/all
-tubeup --use-download-archive http://twitch.tv/sydsnap/videos/all
-tubeup --use-download-archive http://twitch.tv/textfilesdotcom/videos/all
-tubeup --use-download-archive http://twitch.tv/thedanklank/videos/all
-tubeup --use-download-archive http://twitch.tv/toplesstopics/videos/all
-tubeup --use-download-archive http://twitch.tv/yamatoworks/videos/all
-tubeup --use-download-archive http://twitch.tv/zeldacw/videos/all
-tubeup --use-download-archive http://twitch.tv/zonesama/videos/all
+## DEAD CHANNEL ## tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/brittanyventi/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams  --metadata=collection:healthyaddictvideos http://twitch.tv/fakegamergirl/videos/all 
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/SashaHippie/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/afoxinspaceshow/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/charlscarroll/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/chubbykittenreviews/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/gamecatt/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/gunsarefun911/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/hydeart/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/jellykin/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/kimimoons/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/kristiplays/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/kyleeasakura/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/ryuudog_/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/shadbasemurdertv/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/spoonyone/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/sydsnap/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/textfilesdotcom/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/thedanklank/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/toplesstopics/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/yamatoworks/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/zeldacw/videos/all
+tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/zonesama/videos/all
