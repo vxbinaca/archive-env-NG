b5b2408997437145918ce69d158c5e31fc69eb54
Your Name <vxbinaca@protonmail.ch>
2018-08-22 07:52:07 +0000

    Deactivated dead target, added new one, sorted

--- targets/vimeo.sh
+++ targets/vimeo.sh
@@ -1,9 +1,10 @@
+## tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/junnnktank ## DEAD
 ##tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/toplesstopics
 ##tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user8923725
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/exey
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/fetishfactory
-tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/junnnktank
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/kuvshinovilya
+tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/mrtello
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/sukaoff
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user1944739
 tubeup --use-download-archive --metadata=collection:tubeup http://vimeo.com/user24127837
