9b831a77946958ad1f40df8cf830d11b05724503
Paul Henning <vxbinaca@protonmail.ch>
2018-11-01 14:14:51 -0400

    Removed manual licence set

--- targets/naomi.sh
+++ targets/naomi.sh
@@ -1,1 +1,2 @@
-tubeup --use-download-archive --metadata=collection:naomiwuarchive --metadata=licenseurl:http://creativecommons.org/licenses/by-sa/4.0/ http://www.youtube.com/channel/UCh_ugKacslKhsGGdXP0cRRA/videos
+## --metadata=licenseurl:http://creativecommons.org/licenses/by-sa/4.0/ 
+tubeup --use-download-archive --metadata=collection:naomiwuarchive http://www.youtube.com/channel/UCh_ugKacslKhsGGdXP0cRRA/videos
