e2d72efefdb8d51001f735c7f263c4d8ba41d28b
Paul Henning <vxbinaca@protonmail.ch>
2018-11-04 02:13:23 -0500

    Created items script

---
+++ survey/items.sh
@@ -0,0 +1,6 @@
+ia search --itemlist 'collection:kigvideos' >> doller-items.txt
+ia search --itemlist 'collection:sargonyoutube' >> sargon-items.txt
+ia search --itemlist 'collection:ytpmv-mad' >> ytpmv-items.txt
+ia search --itemlist 'collection:veevideos' >> vee-items.txt
+ia search --itemlist 'collection:weedtubers' >> stoner-items.txt
+ia search --itemlist 'collection:louislevauvideos' >> louis-items.txt
