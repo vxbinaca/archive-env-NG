3cac5de7dd91bcc0f33d26795f5d40b240dda7c2
Paul Henning <vxbinaca@protonmail.ch>
2018-12-26 14:12:28 -0500

    Removed l0de, prcessed remaining URLs

--- targets/lrh-pp4l.sh
+++ targets/lrh-pp4l.sh
@@ -1,4 +1,3 @@
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/r0pav/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/l0de
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/r0pav/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng
