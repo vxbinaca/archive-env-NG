8db610e1027ed84b514eb45484bc63a066be39b9
Paul Henning <vxbinaca@protonmail.ch>
2019-01-31 23:23:39 -0500

    Added WEBM detection

--- missed.sh
+++ missed.sh
@@ -4,6 +4,7 @@ ls *.mp4 >> ~/git/archive-env-NG/.missed ## Append MP4 files to list
 ls *.mkv >> ~/git/archive-env-NG/.missed ## Append MKV files to list
 ls *.part* >> ~/git/archive-env-NG/.missed ## Append PART files to list
 ls *.ytdl* >> ~/git/archive-env-NG/.missed ## Append YTDL files to list
+ls *.webm* >> ~/git/archive-env-NG/.missed ## Append WEBM files to list
 cd ~/git/archive-env-NG/
 cat .missed | sort -u > .missed2 ; cat .missed2 > .missed ; rm .missed2 
 git add .missed ; git commit -m "Updated leftover failed uploads"
