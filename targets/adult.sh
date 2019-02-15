4bfecb6997a935e5da9fa167be896e17bb1797d4
Paul Henning <vxbinaca@protonmail.ch>
2019-02-15 16:57:20 -0500

    Created adult industry script

---
+++ targets/adult.sh
@@ -0,0 +1,4 @@
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCe-R3nqJVdW-xWV5rfcbK7A
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/emmnetwork
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCgqXdQDUUynay5zG3pmLWTw
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCPcTxQpqGT6JQxZ0zVnZ13w
