1a2cbdf3818082f4e058bcbf14cea1e72d59d727
Paul Henning <vxbinaca@protonmail.ch>
2019-02-28 11:01:56 -0500

    Added various targets

--- targets/adult.sh
+++ targets/adult.sh
@@ -1,4 +1,12 @@
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCe-R3nqJVdW-xWV5rfcbK7A
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/emmnetwork
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCgqXdQDUUynay5zG3pmLWTw
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCPcTxQpqGT6JQxZ0zVnZ13w
+https://www.youtube.com/channel/UCe-R3nqJVdW-xWV5rfcbK7A
+https://www.youtube.com/user/emmnetwork
+https://www.youtube.com/channel/UCgqXdQDUUynay5zG3pmLWTw
+https://www.youtube.com/channel/UCPcTxQpqGT6JQxZ0zVnZ13w
+https://www.youtube.com/channel/UCypv3gnAf-Ww4-iJ6fYc0cg
+https://www.youtube.com/channel/UCOOIibe-CCdC0VWlvCjf0qw
+https://www.youtube.com/channel/UCyWnfUZHcgVxBS3wAUGA15A
+https://www.youtube.com/user/officialbreeolson9
+https://www.youtube.com/channel/UCS5jTl5mDs2XY3eR9XkpUtQ
+https://www.youtube.com/channel/UCTulpxDN56BLbhqiyqQgFHg
+https://www.youtube.com/channel/UCLLi4qLYWPD2mYb-niJLy5w
+
