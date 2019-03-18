7ad8aee2534d9a34a50d463120968c804c248c24
Paul Henning <vxbinaca@protonmail.ch>
2019-03-18 07:55:50 -0400

    Added Gamr SFM

--- targets/sfm.sh
+++ targets/sfm.sh
@@ -1,6 +1,7 @@
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ
-## tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/HypotheDragon ### Terminated
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/user/COBMetalHead
-tubeup --use-download-archive --metadata=collection:tubeup https://www.youtube.com/user/tf2thewinglet
-tubeup --use-download-archive --metadata=collection:tubeup http://www.youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg
+## tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/HypotheDragon ### Terminated
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC0PRAmDHDdcfPLlEVUoNs0w
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/COBMetalHead
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/tf2thewinglet
