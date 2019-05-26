075fbdf285488f15f7f616008a64901416801f07
Paul Henning <vxbinaca@protonmail.ch>
2019-05-26 00:49:42 -0400

    Added Piper Blush other channels

--- targets/var.sh
+++ targets/var.sh
@@ -6,6 +6,7 @@
 #python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/katTcritic ##NJ nerds
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC1fSp9Qpp8tT982OumLS5Hg
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC4QFrwuzxJ2eC7Q2iDrlJFg
+python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC4kWnf_8zo0KX5oYHRaozDw ## Piper CoCoScope
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC5hie5xBr7qENWVD_kfsiew
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC7yPuRdd4ZKhJQueiVnjd2A
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC8aG3LDTDwNR1UQhSn9uVrw
@@ -18,16 +19,19 @@ python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://y
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCF30c7L3SiutEP9m_HhBZAw
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCG5ZWGfgDXk40t9Og02F5yA
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCGCqGI3f2WSDnfRjrD1Cj_Q
+python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCImh042Sqo5MilDYye2dU_A ## Piper Blush
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCKNQ5RbMm42xVVARTJoEsIg
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCKcibNqpgyXwInQHS4s_I4Q
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCKlA7qF9XKwu79ULYmVu28w
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCLGRZ0xSaBMIUmtn8wkPP3w
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCNEo4Lb14UTkcqBZ9nKwyTg
+python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCQSQgkFf00NO0ahl1cpJ1JQ ## Today With Piper
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCS6F1cpcSoJkztme19_KIzw
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCSuHzQ3GrHSzoBbwrIq3LLA
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCT5y7BAocgM-PeXpVCvyG4g
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCTRpJwRhE-uifL7fTRVe48g
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCTlQFIx8RUus4R3zYAUHHYA
+python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCUKihB_Y4_CIHSWCEBsId-w ## ici piper
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWH4JkQWioU3vTAritHDnAA
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWMPmKJ8GmmUvI5ZC0V2VsA
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCZZFrPKyH-Le2uoPGnn5jyg
@@ -36,6 +40,7 @@ python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://y
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCdjLHBxJIJmJ4srj0NYFl8g
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCdpkbiiCFHVPZrSYEEWyDdw
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCgHUzh9T2DHvSDZcnILqH8g
+python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCgIijLIjrVWwmWch8Q1XMYg ## Piper Will It Fit
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCjPao-PMHoC1IBurivhfGFQ
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCkDXV1ELQBVV4BuLR-FyfkA
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCkfqhn4lyofss8PM1IubDgA
@@ -74,14 +79,13 @@ python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://y
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/shachiNT
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/stevieryan
 python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/veradevera 
+python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UC5ihB7WP6MtZbB3XyV2vj9Q ## Skeptic Feminist
 python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UC6wdP5hTf_phre6Q3kFiV2g
+python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UCL_f53ZEJxp8TtlOkHwMV9Q ## Jordan Peterson
+python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UCT7BLBDnD-wEXeqZSg24aJw ## Soph
 python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UC_1HVMnw-610qx54iEiWk7A
 python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UCrvmV0HaVXnsS6EcN_fdmTg
+python3.7 -m tubeup --use-download-archive http://youtube.com/user/ClanOfTheGrayWolf
 python3.7 -m tubeup --use-download-archive http://youtube.com/user/Shoe0ffHead
 python3.7 -m tubeup --use-download-archive http://youtube.com/user/Shoe0nHead
-python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UC5ihB7WP6MtZbB3XyV2vj9Q ## Skeptic Feminist
 python3.7 -m tubeup --use-download-archive http://youtube.com/user/smiletone ## Gazi Kodzo
-python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UCL_f53ZEJxp8TtlOkHwMV9Q ## Jordan Peterson
-python3.7 -m tubeup --use-download-archive http://youtube.com/user/ClanOfTheGrayWolf
-python3.7 -m tubeup --use-download-archive http://youtube.com/channel/UCT7BLBDnD-wEXeqZSg24aJw ## Soph
-python3.7 -m tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCImh042Sqo5MilDYye2dU_A ## Piper Blush
