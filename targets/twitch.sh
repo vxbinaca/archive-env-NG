fee9f21d1cd331be747629ca98cc49b670d10a54
Paul Henning <vxbinaca@protonmail.ch>
2019-11-03 19:58:52 -0500

    Added Producerchik (Amy) for ingestion

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -18,6 +18,7 @@ python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams h
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/kristiplays/videos/all
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/kyleeasakura/videos/all
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/nyannyancosplay/videos/all
+python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/producerchik/all
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/r0x4nn3/videos/all
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/ryuudog_/videos/all
 python3.7 -m tubeup --use-download-archive --metadata=collection:twitchstreams http://twitch.tv/sae_jin/videos/all
