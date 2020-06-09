79d729aaac981be0f4d9dd46ab9287141c10a46a
Your Name <vxbinaca@protonmail.ch>
2020-06-09 21:11:22 +0000

    Optimized

--- missed.sh
+++ missed.sh
@@ -1,10 +1,7 @@
-cd ~/.tubeup/downloads/
-ls *.mp4 > ~/git/archive-env-NG/.missed ## Append MP4 files to list
-ls *.mkv >> ~/git/archive-env-NG/.missed ## Append MKV files to list
-ls *.part* >> ~/git/archive-env-NG/.missed ## Append PART files to list
-ls *.ytdl* >> ~/git/archive-env-NG/.missed ## Append YTDL files to list
-ls *.webm* >> ~/git/archive-env-NG/.missed ## Append WEBM files to list
-cd ~/git/archive-env-NG/
-cat .missed | sort -u > .missed2 ; cat .missed2 > .missed ; rm .missed2 
+ls ~/.tubeup/downloads/*.mp4 > ~/git/archive-env-NG/.missed ## Append MP4 files to list
+ls ~/.tubeup/downloads/*.mkv >> ~/git/archive-env-NG/.missed ## Append MKV files to list
+ls ~/.tubeup/downloads/*.part* >> ~/git/archive-env-NG/.missed ## Append PART files to list
+ls ~/.tubeup/downloads/*.ytdl* >> ~/git/archive-env-NG/.missed ## Append YTDL files to list
+ls ~/.tubeup/downloads/*.webm* >> ~/git/archive-env-NG/.missed ## Append WEBM files to list
 git add .missed ; git commit -m "Updated leftover failed uploads"
 
