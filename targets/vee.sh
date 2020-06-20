00cba7432db8125f456e72661f388720c4e13ab5
Paul Henning <vxbinaca@protonmail.ch>
2020-06-20 06:20:20 -0400

    Fixed URL

--- targets/vee.sh
+++ targets/vee.sh
@@ -1,3 +1,3 @@
-python3.7 -m tubeup --use-download-archive --metadata=collection:veevideos http://www.youtube.com/user/veemonro/videos
+python3.7 -m tubeup --use-download-archive --metadata=collection:veevideos http://www.youtube.com/user/veemonro
 python3.7 -m tubeup --use-download-archive --metadata=collection:veevideos http://www.youtube.com/channel/UCPOtvnTOJVuqf6U0x5DKWcA
 
