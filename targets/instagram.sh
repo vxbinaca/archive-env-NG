a0d6b782ebefb0955dc62631b47f03f6bd7c6c7d
Paul Henning <vxbinaca@protonmail.ch>
2020-07-13 04:00:02 -0400

    Re-enabled download archive for targets

--- targets/instagram.sh
+++ targets/instagram.sh
@@ -1,32 +1,63 @@
 python3.8 -m tubeup --use-download-archive --metadata=collection:tubeup http://instagram.com/kimimoons/
+sleep 5m
 python3.8 -m tubeup --use-download-archive --metadata=collection:tubeup http://instagram.com/laurenjadeburrowxoxo/
+sleep 5m
 python3.8 -m tubeup --use-download-archive --metadata=collection:tubeup http://instagram.com/monroesirena/
+sleep 5m
 python3.8 -m tubeup --use-download-archive --metadata=collection:tubeup http://instagram.com/roarie_yum/
+sleep 5m
 python3.8 -m tubeup --use-download-archive --metadata=collection:tubeup http://instagram.com/xxdorkaliciousxx/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/_flameo_/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/candystripeconcentrates/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/f.rancesrae/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/h4ley20/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/hannahsweedworld/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/hazelverse/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/jennywakeandbake/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/kimmycosmetics/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/kimmytanofficial/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/kimmytantattoo/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/ktbean420/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/kushbushcrits/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/kyrstun.irene/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/leafly/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/marniejuana/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/meangreenqueen/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/nataliefour20/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/positivesmash/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/positivesmash420/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/qaradisiak/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/ripmyoldaccount/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/satansselfie/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/silencedhippie/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/stoneyspike/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/t00pale.2mg/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/thejoyaride/
+sleep 5m 
 python3.8 -m tubeup --use-download-archive --metadata=collection:weedtubers http://instagram.com/theweedtube/
