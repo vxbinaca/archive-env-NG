5a5b0f96ce060339043b1a0068fbc4505fca9254
vxbinca <vxbinaca@protonmail.ch>
2021-09-11 19:14:50 +0200

    Added targets, fixed URLs

--- targets/lrh-pp4l.sh
+++ targets/lrh-pp4l.sh
@@ -1,3 +1,4 @@
 tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/r0pav/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC_waCFCtUJpkkUxftNCbOBA/videos
