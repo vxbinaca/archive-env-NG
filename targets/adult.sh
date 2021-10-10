994114da7a46bb1f098ef93b669cb4c6e6870e47
vxbinaca <vxbinaca@protonmail.ch>
2021-10-10 11:45:44 -0400

    Added targets, sorted

--- targets/adult.sh
+++ targets/adult.sh
@@ -1,11 +1,17 @@
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/c/RileyReidx3/videos
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UC071DPKFZzOf-GkGZ7-zbGQ/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UC0_TARSGSOrKtjSeyKIM6wA/videos
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UC5Bwuy_YTOB7YbOPrkb9vVg/videos ## Fetties
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCLLi4qLYWPD2mYb-niJLy5w/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCOOIibe-CCdC0VWlvCjf0qw/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCPcTxQpqGT6JQxZ0zVnZ13w/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCS5jTl5mDs2XY3eR9XkpUtQ/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCTulpxDN56BLbhqiyqQgFHg/videos
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCVyohBi1KHjyUooB_8nXhDg/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCWMPmKJ8GmmUvI5ZC0V2VsA/videos ## Mercedes Carrera
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCXjkPQN193ToX487P6Bcc0A/videos
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCXvKUavCtDOlA8bT1i2tI3w/videos ## Belle Delphine
+tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UC_j65wgy_u3mycNCn_7D2Fw/videos ## Indigo White
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCe-R3nqJVdW-xWV5rfcbK7A/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCgqXdQDUUynay5zG3pmLWTw/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/channel/UCoRmKIwJAUn_qz1sbS4R94Q/videos
@@ -22,9 +28,3 @@ tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.c
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/user/modelmayhemofficial/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/user/officialbreeolson9/videos
 tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt http://youtube.com/user/xoClaireRobbins/videos
-tubeup --use-download-archive --metadata=collection:tubeup --cookies ~/youtube.com_cookies.txt https://www.youtube.com/channel/UC071DPKFZzOf-GkGZ7-zbGQ/videos
-tubeup --use-download-archive --cookies ~/youtube.com_cookies.txt http://youtube.com/c/RileyReidx3/videos
-tubeup --use-download-archive --cookies ~/youtube.com_cookies.txt tubeup --use-download-archive http://youtube.com/channel/UC5Bwuy_YTOB7YbOPrkb9vVg/videos ## Fetties
-tubeup --use-download-archive --cookies ~/youtube.com_cookies.txt tubeup --use-download-archive http://youtube.com/channel/UCXvKUavCtDOlA8bT1i2tI3w/videos ## Belle Delphine
-tubeup --use-download-archive --cookies ~/youtube.com_cookies.txt tubeup --use-download-archive http://youtube.com/channel/UC_j65wgy_u3mycNCn_7D2Fw/videos ## Indigo White
-tubeup --use-download-archive --cookies ~/youtube.com_cookies.txt tubeup --use-download-archive http://youtube.com/channel/UCVyohBi1KHjyUooB_8nXhDg/videos
