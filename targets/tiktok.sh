9083918e28653fe38e8ec31575a6ae8c9d33cc14
vxbinaca <vxbinaca@protonmail.ch>
2022-01-30 10:16:51 -0500

    Added emo chick

--- targets/tiktok.sh
+++ targets/tiktok.sh
@@ -1,3 +1,4 @@
 tubeup --use-download-archive http://tiktok.com/@wish_rathod
 tubeup --use-download-archive http://tiktok.com/@emipexu
 tubeup --use-download-archive http://tiktok.com/@evaaviolett
+tubeup --use-download-archive http://tiktok.com/@rexmustdie
