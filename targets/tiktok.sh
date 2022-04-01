d753d278955cae6a05fc1757e0e7873b8b1eeac1
vxbinaca <vxbinaca@protonmail.ch>
2022-03-31 23:37:21 -0400

    normalize

--- targets/tiktok.sh
+++ targets/tiktok.sh
@@ -1,5 +1,6 @@
-tubeup --use-download-archive http://tiktok.com/@wish_rathod
-tubeup --use-download-archive http://tiktok.com/@emipexu
-tubeup --use-download-archive http://tiktok.com/@evaaviolett
-tubeup --use-download-archive http://tiktok.com/@rexmustdie
-tubeup --use-download-archive http://tiktok.com/@another__femboy
+tubeup http://tiktok.com/@wish_rathod
+tubeup http://tiktok.com/@emipexu
+tubeup http://tiktok.com/@evaaviolett
+tubeup http://tiktok.com/@rexmustdie
+tubeup http://tiktok.com/@another__femboy
+tubeup http://tiktok.com/@fegalvao_
