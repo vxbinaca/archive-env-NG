168ad3d687d187a8a35b9844d251483a784810ad
vxbinaca <vxbinaca@protonmail.ch>
2022-04-11 13:14:36 -0400

    Added target, sorted

--- targets/twitch.sh
+++ targets/twitch.sh
@@ -161,3 +161,4 @@ tubeup --use-download-archive --metadata=collection:twitchstreams "http://twitch
 tubeup --use-download-archive --metadata=collection:twitchstreams "http://twitch.tv/yuninja8/videos?filter=archives&sort=time"
 tubeup --use-download-archive --metadata=collection:twitchstreams "http://twitch.tv/zeldacw/videos?filter=archives&sort=time"
 tubeup --use-download-archive --metadata=collection:twitchstreams "http://twitch.tv/zonesama/videos?filter=archives&sort=time"
+tubeup --use-download-archive --metadata=collection:twitchstreams "https://www.twitch.tv/asagi_t/videos\?filter\=all"
