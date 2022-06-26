083c22b3d509580c213a79e0e6564b2a7a555fee
vxbinaca <vxbinaca@protonmail.ch>
2022-06-25 20:50:24 -0400

    Removed Hyperkin

--- targets/sargon.sh
+++ targets/sargon.sh
@@ -2,4 +2,3 @@ tubeup --use-download-archive --metadata=collection:sargonyoutube http://www.you
 tubeup --use-download-archive --metadata=collection:sargonyoutube http://www.youtube.com/channel/UCSMG40p6XHFTeXv5noF6Wdw/videos
 tubeup --use-download-archive --metadata=collection:sargonyoutube http://www.youtube.com/channel/UCpiCH7qvGVlzMOqy3dncA5Q/videos
 tubeup --use-download-archive --metadata=collection:sargonyoutube http://www.youtube.com/user/SargonofAkkad100/videos
-tubeup --use-download-archive --metadata=collection:sargonyoutube https://www.youtube.com/channel/UCvPHJP6EnHEjyMi9Rf1jYMw/videos
