991649a0eae486ad7bd145f017a1311f89aa2da5
vxbinaca <vxbinaca@protonmail.ch>
2022-07-15 22:42:47 -0400

    added targets, sorted

--- targets/tiktok.sh
+++ targets/tiktok.sh
@@ -1,7 +1,8 @@
-tubeup http://tiktok.com/@wish_rathod
+tubeup http://tiktok.com/@another__femboy
+tubeup http://tiktok.com/@devilishbri
 tubeup http://tiktok.com/@emipexu
 tubeup http://tiktok.com/@evaaviolett
-tubeup http://tiktok.com/@rexmustdie
-tubeup http://tiktok.com/@another__femboy
 tubeup http://tiktok.com/@fegalvao_
-tubeup http://tiktok.com/@devilishbri
+tubeup http://tiktok.com/@rexmustdie
+tubeup http://tiktok.com/@vanessallen2001
+tubeup http://tiktok.com/@wish_rathod
