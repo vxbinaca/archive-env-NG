360b831442354a5901125175d41cc3115378dbaf
vxbinaca <vxbinaca@protonmail.ch>
2022-08-06 18:34:55 -0400

    flag change

--- targets/sfm.sh
+++ targets/sfm.sh
@@ -1,8 +1,8 @@
-## tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/HypotheDragon ### Terminated
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC0PRAmDHDdcfPLlEVUoNs0w/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCOFp1Y8QmsDXREfK8NZag1g/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/COBMetalHead/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/tf2thewinglet/videos
+## tubeup --metadata=collection:tubeup http://youtube.com/user/HypotheDragon ### Terminated
+tubeup --metadata=collection:tubeup http://youtube.com/channel/UC0PRAmDHDdcfPLlEVUoNs0w/videos
+tubeup --metadata=collection:tubeup http://youtube.com/channel/UCOFp1Y8QmsDXREfK8NZag1g/videos
+tubeup --metadata=collection:tubeup http://youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ/videos
+tubeup --metadata=collection:tubeup http://youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg/videos
+tubeup --metadata=collection:tubeup http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew/videos
+tubeup --metadata=collection:tubeup http://youtube.com/user/COBMetalHead/videos
+tubeup --metadata=collection:tubeup http://youtube.com/user/tf2thewinglet/videos
