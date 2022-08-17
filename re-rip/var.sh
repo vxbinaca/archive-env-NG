a41eec10f23177f0060747daa528a5d7dd7e3254
Paul H <vxbinaca@protonmail.ch>
2022-08-16 20:02:48 -0400

    staging re-rip

---
+++ re-rip/var.sh
@@ -0,0 +1,169 @@
+ yt-dlp --get-id http://youtube.com/c/MoLupo21 >> IDs.sh # Wos X
+ yt-dlp --get-id http://youtube.com/channel/UC-zgHpR-_niRxow6vS6nSGQ >> IDs.sh ## Mellyboo
+ yt-dlp --get-id http://youtube.com/channel/UC1fSp9Qpp8tT982OumLS5Hg >> IDs.sh ## Tactical Bacon Patches
+ yt-dlp --get-id http://youtube.com/channel/UC27m3R6W5sP7-QLfMTEso2w >> IDs.sh ## 全てあなたの所為です。
+ yt-dlp --get-id http://youtube.com/channel/UC48YM-rBdkGCHs2fWpuY7hg >> IDs.sh ## Female teacher practicing cooking
+ yt-dlp --get-id http://youtube.com/channel/UC4QFrwuzxJ2eC7Q2iDrlJFg >> IDs.sh ## dosgatosmuertos
+ yt-dlp --get-id http://youtube.com/channel/UC4kWnf_8zo0KX5oYHRaozDw >> IDs.sh ## Piper CoCoScope
+ yt-dlp --get-id http://youtube.com/channel/UC5hie5xBr7qENWVD_kfsiew >> IDs.sh ## Digital Citizens Alliance
+ yt-dlp --get-id http://youtube.com/channel/UC7yPuRdd4ZKhJQueiVnjd2A >> IDs.sh ## Joe Capo
+ yt-dlp --get-id http://youtube.com/channel/UCAHPaRfB7hqXtdOoqWux4EQ >> IDs.sh ## CursedArachnid
+ yt-dlp --get-id http://youtube.com/channel/UCAo6nC6I9yk2X4xqQThhhew >> IDs.sh ## ZEST AMV
+ yt-dlp --get-id http://youtube.com/channel/UCC8904HNAeMwgLhs58oJjxA >> IDs.sh ## CaitlynPaige
+ yt-dlp --get-id http://youtube.com/channel/UCCxDI0vt1jVqQUd9ksCu5Ag >> IDs.sh ## CadyGamer Pix
+ yt-dlp --get-id http://youtube.com/channel/UCDLA7Y_6mt-9UrrxTuudt8Q >> IDs.sh ## ハイパー道楽
+ yt-dlp --get-id http://youtube.com/channel/UCE4yx7b0-zCtj5QmbFUCvHg >> IDs.sh ## satopanorama
+ yt-dlp --get-id http://youtube.com/channel/UCEAqHbeIpPERhniNjQyR7EQ >> IDs.sh ## Locked In The Tower
+ yt-dlp --get-id http://youtube.com/channel/UCEG3ulBltfrvOE8baBogJ0g >> IDs.sh ## ZONE TOONS
+ yt-dlp --get-id http://youtube.com/channel/UCEUrQzNPvDQoEMNswAr8mCA >> IDs.sh ## Bambicue
+ yt-dlp --get-id http://youtube.com/channel/UCF30c7L3SiutEP9m_HhBZAw >> IDs.sh ## Alyssa Marie KO
+ yt-dlp --get-id http://youtube.com/channel/UCGCqGI3f2WSDnfRjrD1Cj_Q >> IDs.sh ## Nicole Mayhem
+ yt-dlp --get-id http://youtube.com/channel/UCHGr3Ca27u68X1swTGyrkNQ >> IDs.sh ## Demon Grimalkin
+ yt-dlp --get-id http://youtube.com/channel/UCIY27ReU--8y7rI8wJPvBWg >> IDs.sh ## Miss Moonified
+ yt-dlp --get-id http://youtube.com/channel/UCImh042Sqo5MilDYye2dU_A >> IDs.sh ## Piper Blush
+ yt-dlp --get-id http://youtube.com/channel/UCJil534CSR8m5BeQVPkTx4Q >> IDs.sh ## Kuma Cooking
+ yt-dlp --get-id http://youtube.com/channel/UCKNQ5RbMm42xVVARTJoEsIg >> IDs.sh ## Rescued Film Project
+ yt-dlp --get-id http://youtube.com/channel/UCKcibNqpgyXwInQHS4s_I4Q >> IDs.sh ## Ava Lenore
+ yt-dlp --get-id http://youtube.com/channel/UCKlA7qF9XKwu79ULYmVu28w >> IDs.sh ## TheZZAZZGlitch
+ yt-dlp --get-id http://youtube.com/channel/UCLGRZ0xSaBMIUmtn8wkPP3w >> IDs.sh ## Brittany Venti
+ yt-dlp --get-id http://youtube.com/channel/UCQSQgkFf00NO0ahl1cpJ1JQ >> IDs.sh ## Today With Piper
+ yt-dlp --get-id http://youtube.com/channel/UCSuHzQ3GrHSzoBbwrIq3LLA >> IDs.sh ## Naomi Brockwell
+ yt-dlp --get-id http://youtube.com/channel/UCT5y7BAocgM-PeXpVCvyG4g >> IDs.sh ## Hal (metokur channel)
+ yt-dlp --get-id http://youtube.com/channel/UCTRpJwRhE-uifL7fTRVe48g >> IDs.sh ## T-DJ Milana
+ yt-dlp --get-id http://youtube.com/channel/UCTlQFIx8RUus4R3zYAUHHYA >> IDs.sh ## Dutchko
+ yt-dlp --get-id http://youtube.com/channel/UCUKihB_Y4_CIHSWCEBsId-w >> IDs.sh ## ici piper
+ yt-dlp --get-id http://youtube.com/channel/UCVmLGSIez0Oay3bsTmZbIUQ >> IDs.sh ## ThePuneya
+ yt-dlp --get-id http://youtube.com/channel/UCVy5jg4ryacDTWXXpV035fg >> IDs.sh ## Stefania Ferrario
+ yt-dlp --get-id http://youtube.com/channel/UCXwCE2sF1TRnaWfmEMlz8vA >> IDs.sh ## For Men
+ yt-dlp --get-id http://youtube.com/channel/UC_CKL5F9i7e9sPnZcxXP3HQ >> IDs.sh ## Sherif Gaber
+ yt-dlp --get-id http://youtube.com/channel/UCaIg15JhvGgPwdurGNqjJUg >> IDs.sh ## buzz.my.woody
+ yt-dlp --get-id http://youtube.com/channel/UCdjLHBxJIJmJ4srj0NYFl8g >> IDs.sh ## ZimoNitrome 2
+ yt-dlp --get-id http://youtube.com/channel/UCfirnXRfZlrZ_X36npwaE3A >> IDs.sh ## Inconvenient Truths by Jennifer Zeng曾錚真言
+ yt-dlp --get-id http://youtube.com/channel/UCgHUzh9T2DHvSDZcnILqH8g >> IDs.sh ## Dehydrated Princess
+ yt-dlp --get-id http://youtube.com/channel/UCgIijLIjrVWwmWch8Q1XMYg >> IDs.sh ## Piper Will It Fit
+ yt-dlp --get-id http://youtube.com/channel/UCjPao-PMHoC1IBurivhfGFQ >> IDs.sh ## The Wild Bunch -Knob Creek-
+ yt-dlp --get-id http://youtube.com/channel/UCkDXV1ELQBVV4BuLR-FyfkA >> IDs.sh ## YesBoobs.com
+ yt-dlp --get-id http://youtube.com/channel/UCkfqhn4lyofss8PM1IubDgA >> IDs.sh ## JAPALATINA
+ yt-dlp --get-id http://youtube.com/channel/UClMLaP99_AGVcJ-E9lvW5kw >> IDs.sh ## Little Miss Anonymous
+ yt-dlp --get-id http://youtube.com/channel/UCla6APLHX6W3FeNLc8PYuvg >> IDs.sh ## Lauren Southern
+ yt-dlp --get-id http://youtube.com/channel/UCmOVEae8Tl7XmdjdxLbJHkw >> IDs.sh ## TeamSESH
+ yt-dlp --get-id http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew >> IDs.sh ## Studio FOW
+ yt-dlp --get-id http://youtube.com/channel/UCnQogRWNwH-m8W0NQMYI3Ig >> IDs.sh ## Extra Spooky
+ yt-dlp --get-id http://youtube.com/channel/UCox0RC7bP_wafzpRmlnD9EQ >> IDs.sh ## The OG Underboobery
+ yt-dlp --get-id http://youtube.com/channel/UCp8tB7kfhjvmgY2wUDBlaMA >> IDs.sh ## sonyericsson
+ yt-dlp --get-id http://youtube.com/channel/UCpMMcYVSnqOIJA11vBnFngQ >> IDs.sh ## David Hogg
+ yt-dlp --get-id http://youtube.com/channel/UCroYqE1LddWoe1L3gZEqUXg >> IDs.sh ## Chubby Kitten Gaming
+ yt-dlp --get-id http://youtube.com/channel/UCs9WfIvLdL0WSAFaRl9FOAQ >> IDs.sh ## Beige Frequency
+ yt-dlp --get-id http://youtube.com/channel/UCvNsW_rz1kj2KiM1TJU6E5Q >> IDs.sh ## Ivy Lee
+ yt-dlp --get-id http://youtube.com/user/0dutch >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/BraveTheWorldChannel >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/DNALounge >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/DXLiberty >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/HiMyNameIsRoxi >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/LeoPirate >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/NeutralNerdBird >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/Ooolalaa >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/ProfessorFennec >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/RareNargacuga >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/TLCSprout >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/TheRealParis >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/ZimoNitrome >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/acourtroom >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/debuglive >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/dsscdse >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/etanthe4th >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/flippingpages1014 >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/fuzzyfunction >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/hezuneutral >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/komori841 >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/littleloca >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/lonelygirl15 >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/nitengotv >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/pikopikoken >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/shachiNT >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/stevieryan >> IDs.sh 
+ yt-dlp --get-id http://youtube.com/user/veradevera  >> IDs.sh 
+# yt-dlp --get-id http://youtube.com/channel/UCB5Yw6r20bTrkZZDS_EnNvg ## DEAD
+# yt-dlp --get-id http://youtube.com/channel/UCG5ZWGfgDXk40t9Og02F5yA ## TERMINATED
+# yt-dlp --get-id http://youtube.com/channel/UCKH9HfYY_GEcyltl2mbD5lA ##NJ nerds
+# yt-dlp --get-id http://youtube.com/channel/UCNEo4Lb14UTkcqBZ9nKwyTg ## DOES NOT EXIST
+# yt-dlp --get-id http://youtube.com/channel/UCS6F1cpcSoJkztme19_KIzw ## DOES NOY EXIST
+# yt-dlp --get-id http://youtube.com/channel/UCZZFrPKyH-Le2uoPGnn5jyg ## TERMINATED
+# yt-dlp --get-id http://youtube.com/channel/UC_-9rxe8VZI-tUQJWEig4mA ##NJ nerds
+# yt-dlp --get-id http://youtube.com/channel/UCdpkbiiCFHVPZrSYEEWyDdw ## TERMINATED
+# yt-dlp --get-id http://youtube.com/channel/UCnbUeHmpeHAX3xED4fRXKeg ## Vaporwave
+# yt-dlp --get-id http://youtube.com/user/ejmillennium ##NJ nerds
+# yt-dlp --get-id http://youtube.com/user/gotenks6 ##NJ nerds
+# yt-dlp --get-id http://youtube.com/user/katTcritic ##NJ nerds
+## yt-dlp --get-id http://youtube.com/channel/UC3Sm9GeLi1wDRToAuXLJ0dQ >> IDs.sh ## Spicy Rice gaming
+tubeup --use-download-archive --metadata=collection:sarkeesianvideos http://youtube.com/user/feministfrequency >> IDs.sh ## Anita Sarkeesian channel/collection
+yt-dlp --get-id http://youtube.com/channel/UClvrch6lhUO4bDH3ARSdLXw ## えむゆみカップル Emuyumi Couple
+tubeup --use-download-archive http://youtube.com/c/ChapoTrapHouse >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/DimitriMonroeStreams >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/Izzzyzzz >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/JamesBear420_lol >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/Lewdcast >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/c/MichelleAKJx ## 米雪MichelleAKJ
+tubeup --use-download-archive http://youtube.com/c/NakedWanderings >> IDs.sh ## Nick & Lins
+tubeup --use-download-archive http://youtube.com/c/Octav1usExtra >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/c/OverlordGaming >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/TheAdventuresOfAlaska ## Alaska Zade
+tubeup --use-download-archive http://youtube.com/c/bcflyers2016 >> IDs.sh ## BCflyers
+tubeup --use-download-archive http://youtube.com/c/dannphan29online >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/loserlexii >> IDs.sh
+tubeup --use-download-archive http://youtube.com/c/senzawa >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UC071DPKFZzOf-GkGZ7-zbGQ >> IDs.sh ## The Donkey Show
+tubeup --use-download-archive http://youtube.com/channel/UC2wmC89IGdayr9a7EJhxf6w >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/channel/UC4q2dwWldl6dlYTXZqi7yaQ >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UC4unV5BVmWubfAF0Al_AVdw >> IDs.sh ## SyrianGirlpartisan
+tubeup --use-download-archive http://youtube.com/channel/UC5ihB7WP6MtZbB3XyV2vj9Q >> IDs.sh ## Skeptic Feminist
+tubeup --use-download-archive http://youtube.com/channel/UC5o8__B2BXoqFf0Et1fMSrQ >> IDs.sh ## June Lovejoy
+tubeup --use-download-archive http://youtube.com/channel/UC6e-wMTFtTJo-PPY0ZsfbvQ >> IDs.sh ## 麗癒ちゃんねる
+tubeup --use-download-archive http://youtube.com/channel/UC7RYl7VNkiUssSr028azf7w >> IDs.sh ## WWII Girl
+tubeup --use-download-archive http://youtube.com/channel/UCAG1ABZP-c7wuNt0fziHtsA >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCB3v0wy8_PkvQKhNZ8QSWoA ## Forest Mommy
+tubeup --use-download-archive http://youtube.com/channel/UCBhnI7_z3NIJmneNe7wjlPw >> IDs.sh ## Velvet Tube 벨벳튜브
+tubeup --use-download-archive http://youtube.com/channel/UCEQ3pEklukS8Wp6FygeM89Q >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/channel/UCHRdn2I1BI4Q_yLw-QVbkVQ >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCIE1-AjN5_Kf0naxI94qmzQ >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCIqyFzDuFim0B_UaKROeGqw >> IDs.sh ## Run Girls, Run!公式チャンネル
+tubeup --use-download-archive http://youtube.com/channel/UCJil534CSR8m5BeQVPkTx4Q >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCLGRZ0xSaBMIUmtn8wkPP3w >> IDs.sh ## Brittany Venti
+tubeup --use-download-archive http://youtube.com/channel/UCL_f53ZEJxp8TtlOkHwMV9Q >> IDs.sh ## Jordan Peterson
+tubeup --use-download-archive http://youtube.com/channel/UCOBFhDxo5JKjaTzGotRntdA >> IDs.sh ## Muni Bug
+tubeup --use-download-archive http://youtube.com/channel/UCOh33eMeb5Ids_nteQkp_2w ## Modelo Julinha
+tubeup --use-download-archive http://youtube.com/channel/UCOh33eMeb5Ids_nteQkp_2w >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCSRfRHYTw9eeZ-65dX1s_nQ >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCT7BLBDnD-wEXeqZSg24aJw >> IDs.sh ## Soph
+tubeup --use-download-archive http://youtube.com/channel/UCTfzSx9QCk0LJ_-x6XveeMw >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCVlN6QBfwoply9RhxmbBryw >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCWH4JkQWioU3vTAritHDnAA >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/channel/UCXvKUavCtDOlA8bT1i2tI3w >> IDs.sh ## Belle Delphine
+tubeup --use-download-archive http://youtube.com/channel/UCZTTxoM7GT-NVe52xqP0cXw >> IDs.sh ## Ana Isabel
+tubeup --use-download-archive http://youtube.com/channel/UC_1HVMnw-610qx54iEiWk7A >> IDs.sh ## Aki Dearest
+tubeup --use-download-archive http://youtube.com/channel/UC_BEq1haIVw4p25K95kaS3Q >> IDs.sh ## emu.cooking
+tubeup --use-download-archive http://youtube.com/channel/UCcZWQSE5AedwUs-qAJXTaPA ## Normalizing Nudity
+tubeup --use-download-archive http://youtube.com/channel/UCekXK9WdaAbA5YhHX04TPmA >> IDs.sh ## Anastasie Vaskevich
+tubeup --use-download-archive http://youtube.com/channel/UCfHJ5Y3akQ7LA0PQmSYlYmQ >> IDs.sh ## Paralellni Polis
+tubeup --use-download-archive http://youtube.com/channel/UCjeGB0SaIRYJhKY3Vc4A3pQ >> IDs.sh ## GamerGirlRoxy
+tubeup --use-download-archive http://youtube.com/channel/UClYQwz4pVcuDnHXFsjXhbAQ >> IDs.sh ## Krystallize is Back
+tubeup --use-download-archive http://youtube.com/channel/UCnzq0TEm7vMLUe4wEumA39A >> IDs.sh ## Spooky Weeb Trash
+tubeup --use-download-archive http://youtube.com/channel/UCo6hJKM6MvohbS8CNNsxHzg >> IDs.sh
+tubeup --use-download-archive http://youtube.com/channel/UCoLCf3aVRMSukwetHfn1p1A >> IDs.sh ## ボートレース戸田 ## Having rip issues
+tubeup --use-download-archive http://youtube.com/channel/UCobGjlohg1Mtev0uQXflmKQ >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/channel/UCrfroioyR188GNllgeDV8KA >> IDs.sh ## チャンネルいにゃ
+tubeup --use-download-archive http://youtube.com/channel/UCrvmV0HaVXnsS6EcN_fdmTg >> IDs.sh ## Cami Carr
+tubeup --use-download-archive http://youtube.com/channel/UCsIAMpI9bRV9T9gJT9Mbdpw >> IDs.sh ## さえなるチャンネル
+tubeup --use-download-archive http://youtube.com/channel/UCvPHJP6EnHEjyMi9Rf1jYMw >> IDs.sh ## Hyperkin
+tubeup --use-download-archive http://youtube.com/channel/UCxB7iKYaPv8AJBMWXTbZQVQ >> IDs.sh ## Yolo Swag Studios
+tubeup --use-download-archive http://youtube.com/channel/UCxdkQN7XpdzWw9ii2bl91NA >> IDs.sh ## Qwerticular
+tubeup --use-download-archive http://youtube.com/channel/UCy-tM_x3EJETWlqOPRNHeaA >> IDs.sh ## Naked Gardener
+tubeup --use-download-archive http://youtube.com/channel/UCz5Kx6dP6rHRf9yLY1tnelw >> IDs.sh ## Octavius King
+tubeup --use-download-archive http://youtube.com/user/ClanOfTheGrayWolf >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/user/OtakuVsNotaku >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/user/Shoe0ffHead >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/user/Shoe0nHead >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/user/ThePopeB4G >> IDs.sh 
+tubeup --use-download-archive http://youtube.com/user/ayananishinaga >> IDs.sh ## 西永彩奈ちゃんねる
+tubeup --use-download-archive http://youtube.com/user/smiletone >> IDs.sh ## Gazi Kodzo
+tubeup --use-download-archive http://youtube.com/user/sydsnapva >> IDs.sh
