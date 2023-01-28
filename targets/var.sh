bbd80a7aeff43d72c455ffe2609977043fac2c02
vxbinaca <vxbinaca@protonmail.ch>
2023-01-28 08:19:10 -0500

    Added targets, sorted

--- targets/var.sh
+++ targets/var.sh
@@ -355,3 +355,4 @@ tubeup --use-download-archive http://youtube.com/user/smiletone ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
 tubeup --use-download-archive http://youtube.com/@five-meo.education/videos
+tubeup --use-download-archive http://youtube.com/@amyleigh7624
