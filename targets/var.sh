0df38f8fe3dafb464a7fd70f015aefcd73eed242
vxbinaca <vxbinaca@protonmail.ch>
2023-05-06 01:11:50 -0400

    Added targets, sorted

--- targets/var.sh
+++ targets/var.sh
@@ -386,3 +386,4 @@ tubeup --use-download-archive http://youtube.com/user/hotforwords/videos
 tubeup --use-download-archive http://youtube.com/user/smiletone/videos ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
+tubeup --use-download-archive https://www.youtube.com/@Ensemble_GAP/videos
