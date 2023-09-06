b827778f9f02a71ec4c60e976a5c0f7cd001ad1b
vxbinaca <vxbinaca@protonmail.ch>
2023-09-06 09:18:55 -0400

    Fixed URLs

--- targets/kraut.sh
+++ targets/kraut.sh
@@ -1,3 +1,2 @@
-tubeup --use-download-archive --metadata=collection:krautandtea http://www.youtube.com/channel/UCr_Q-bPpcw5fJ-Oow1BW1NQ/videos
-tubeup --use-download-archive --metadata=collection:krautandtea http://www.youtube.com/channel/UCf6G3eRxTfWAgrg5053Iaeg/videos
+tubeup --use-download-archive --metadata=collection:krautandtea http://youtube.com/@Kraut_the_Parrot/videos
 tubeup --use-download-archive --metadata=collection:krautandtea http://www.youtube.com/channel/UCZUzkuF4dd2cHpyOldOT3ww/videos
