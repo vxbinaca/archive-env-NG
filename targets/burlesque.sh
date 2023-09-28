a095eee6396489749ede694f1b26e11404156bf5
vxbinaca <vxbinaca@protonmail.ch>
2023-09-28 12:01:05 -0400

    Merged

--- targets/burlesque.sh
+++
@@ -1,5 +0,0 @@
-tubeup --use-download-archive http://youtube.com/user/MissJennyDoll/videos
-tubeup --use-download-archive http://youtube.com/user/michaelfromberg/videos
-tubeup --use-download-archive http://youtube.com/user/rockinbonesvideo/videos
-tubeup --use-download-archive http://youtube.com/user/VirtuallyNadine/videos
-tubeup --use-download-archive http://youtube.com/channel/UCu61LciL70ZpMruNLzIJUCg/videos
--- targets/var.sh
+++ targets/var.sh
@@ -432,3 +432,8 @@ tubeup --use-download-archive http://youtube.com/user/jkeenanatstanford/videos
 tubeup --use-download-archive http://youtube.com/user/smiletone/videos ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
+tubeup --use-download-archive http://youtube.com/user/MissJennyDoll/videos
+tubeup --use-download-archive http://youtube.com/user/michaelfromberg/videos
+tubeup --use-download-archive http://youtube.com/user/rockinbonesvideo/videos
+tubeup --use-download-archive http://youtube.com/user/VirtuallyNadine/videos
+tubeup --use-download-archive http://youtube.com/channel/UCu61LciL70ZpMruNLzIJUCg/videos
