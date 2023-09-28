f1a791fbb1e791b20c41c854ff3e395957a545e2
vxbinaca <vxbinaca@protonmail.ch>
2023-09-28 12:02:22 -0400

    Merged

--- targets/naomi.sh
+++
@@ -1,2 +0,0 @@
-## --metadata=licenseurl:http://creativecommons.org/licenses/by-sa/4.0/ 
-tubeup --use-download-archive --metadata=collection:naomiwuarchive --metadata=licenseurl:http://creativecommons.org/licenses/by-sa/4.0/ http://www.youtube.com/channel/UCh_ugKacslKhsGGdXP0cRRA/videos
