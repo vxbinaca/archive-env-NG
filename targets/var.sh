8735332218d5a8bfe1b4a93eb7426438f40c6171
vxbinaca <vxbinaca@protonmail.ch>
2024-02-04 19:02:45 -0500

    Added targets, sorted

--- targets/var.sh
+++ targets/var.sh
@@ -467,3 +467,12 @@ tubeup --use-download-archive http://youtube.com/user/rockinbonesvideo/videos
 tubeup --use-download-archive http://youtube.com/user/smiletone/videos ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
+## tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/HypotheDragon ### Terminated
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC0PRAmDHDdcfPLlEVUoNs0w/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCOFp1Y8QmsDXREfK8NZag1g/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCUnMAR2VCFgT7uK62979CsQ/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC_xUE642R1lXtSGuIPhg6gg/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCmyiBfu_idd4fdSQuA6yjew/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/COBMetalHead/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/tf2thewinglet/videos
+tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/@ForceBallFx/videos
