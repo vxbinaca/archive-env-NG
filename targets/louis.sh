58082a40983e79048e75c01880a44f11e10bce18
vxbinaca <vxbinaca@protonmail.ch>
2024-02-04 19:09:52 -0500

    Added targets, sorted

--- targets/louis.sh
+++
@@ -1,2 +0,0 @@
-tubeup --use-download-archive --metadata=collection:louislevauvideos http://www.youtube.com/channel/UC4k3HO5n3aSq_bayF-UHiGg/videos
-tubeup --use-download-archive --metadata=collection:louislevauvideos http://www.youtube.com/channel/UCeMjvwNw3vvsY_nJUG1amFg
--- targets/lrh-pp4l.sh
+++
@@ -1,4 +0,0 @@
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/user/r0pav/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCA9azGE1WiIZAG9PCD5JaYQ/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UCWkdWGNQIYfqgdfhU-59Wng/videos
-tubeup --use-download-archive --metadata=collection:tubeup http://youtube.com/channel/UC_waCFCtUJpkkUxftNCbOBA/videos
