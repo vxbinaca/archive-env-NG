5219816f230559f31d27c2a83599e0b3ab021f83
vxbinaca <vxbinaca@protonmail.ch>
2024-02-19 21:18:41 -0500

    Added targets, sorted

--- targets/var.sh
+++ targets/var.sh
@@ -485,3 +485,5 @@ tubeup --use-download-archive http://youtube.com/user/rockinbonesvideo/videos
 tubeup --use-download-archive http://youtube.com/user/smiletone/videos ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
+tubeup --use-download-archive https://www.youtube.com/@LanaBee/streams
+tubeup --use-download-archive https://www.youtube.com/@LanaBee/videos
