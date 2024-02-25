5d35c8e5fed2b6ea644c7a0aeac32c9759b7a5fa
vxbinaca <vxbinaca@protonmail.ch>
2024-02-25 17:17:01 -0500

    Added Nopel, sorted

--- targets/var.sh
+++ targets/var.sh
@@ -486,5 +486,9 @@ tubeup --use-download-archive http://youtube.com/user/rockinbonesvideo/videos
 tubeup --use-download-archive http://youtube.com/user/smiletone/videos ## Gazi Kodzo
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/shorts
 tubeup --use-download-archive http://youtube.com/user/sydsnapva/videos
-tubeup --use-download-archive https://www.youtube.com/@LanaBee/streams
-tubeup --use-download-archive https://www.youtube.com/@LanaBee/videos
+tubeup --use-download-archive http://youtube.com/@LanaBee/streams
+tubeup --use-download-archive http://youtube.com/@LanaBee/videos
+tubeup --use-download-archive http://youtube.com/@BiancaExciana/streams 
+tubeup --use-download-archive http://youtube.com/@BiancaExciana/videos
+tubeup --use-download-archive http://youtube.com/@BiancaExciana/shorts
+
