cb77c5fd212725aa3ff47336a8d92ad284d8142f
Paul Henning <vxbinaca@protonmail.ch>
2024-03-05 18:54:32 -0500

    Staging overhaul

--- other_scripts/git-arch.sh
+++ other_scripts/git-arch.sh
@@ -1,38 +1,38 @@
-./iagitup.py -u http://github.com/reswitched/CageTheUnicorn
-./iagitup.py -u http://github.com/DavidSM64/Quad64
-./iagitup.py -u http://github.com/soimort/you-get
-./iagitup.py -u http://github.com/makegirlsmoe/makegirls.moe_web
-./iagitup.py -u http://github.com/Aixile/chainer-gan-experiments 
-./iagitup.py -u http://github.com/corna/me_cleaner
-./iagitup.py -u http://github.com/asciimoo/drawille
-./iagitup.py -u http://github.com/davebaol/d2x-cios
-./iagitup.py -u http://github.com/SeattleMeshnet/meshbox
-./iagitup.py -u http://github.com/mogorman/closed_sesame
-./iagitup.py -u http://github.com/TheOfficialFloW/VitaShell
-./iagitup.py -u http://github.com/Rinnegatamante/rinCheat
-./iagitup.py -u http://github.com/Enegnei/JacobAppelbaumLeavesTor
-./iagitup.py -u http://github.com/rdanbrook/nestopia
-./iagitup.py -u http://github.com/christopherpow/nes-test-roms
-./iagitup.py -u http://github.com/henkaku/henkaku
-./iagitup.py -u http://github.com/Fire30/PS4-3.55-Code-Execution-PoC
-./iagitup.py -u http://github.com/torvalds/linux
-./iagitup.py -u http://github.com/dscrts/gay.pl
-./iagitup.py -u http://github.com/furrtek/portapack-havoc
-./iagitup.py -u http://github.com/AdamLaurie/RFIDIOt 
-./iagitup.py -u http://github.com/qntm/base65536
-./iagitup.py -u http://github.com/foosel/OctoPrint
-./iagitup.py -u http://github.com/cryptocat/crypt
-./iagitup.py -u http://github.com/rg3/youtube-dl
-./iagitup.py -u http://github.com/wifiphisher/wifiphisher
-./iagitup.py -u http://github.com/Xyl2k/TSA-Travel-Sentry-master-keys 
-./iagitup.py -u http://github.com/voat/voat
-./iagitup.py -u http://github.com/majestrate/nntpchan
-./iagitup.py -u http://github.com/maduce/fosscad-repo
-./iagitup.py -u http://github.com/nsa-observer/documents
-./iagitup.py -u http://github.com/offensive-security/cryptsetup-nuke-keys
-./iagitup.py -u http://github.com/Bitmessage/PyBitmessage
-./iagitup.py -u http://github.com/jakubroztocil/httpie
-./iagitup.py -u http://github.com/cjb/GitTorrent
-./iagitup.py -u http://github.com/int0x80/anti-forensics
-./iagitup.py -u http://github.com/DanMcInerney/wifijammer
-./iagitup.py -u http://github.com/humhub/humhub
+iagitup http://github.com/reswitched/CageTheUnicorn
+iagitup http://github.com/DavidSM64/Quad64
+iagitup http://github.com/soimort/you-get
+iagitup http://github.com/makegirlsmoe/makegirls.moe_web
+iagitup http://github.com/Aixile/chainer-gan-experiments 
+iagitup http://github.com/corna/me_cleaner
+iagitup http://github.com/asciimoo/drawille
+iagitup http://github.com/davebaol/d2x-cios
+iagitup http://github.com/SeattleMeshnet/meshbox
+iagitup http://github.com/mogorman/closed_sesame
+iagitup http://github.com/TheOfficialFloW/VitaShell
+iagitup http://github.com/Rinnegatamante/rinCheat
+iagitup http://github.com/Enegnei/JacobAppelbaumLeavesTor
+iagitup http://github.com/rdanbrook/nestopia
+iagitup http://github.com/christopherpow/nes-test-roms
+iagitup http://github.com/henkaku/henkaku
+iagitup http://github.com/Fire30/PS4-3.55-Code-Execution-PoC
+iagitup http://github.com/torvalds/linux
+iagitup http://github.com/dscrts/gay.pl
+iagitup http://github.com/furrtek/portapack-havoc
+iagitup http://github.com/AdamLaurie/RFIDIOt 
+iagitup http://github.com/qntm/base65536
+iagitup http://github.com/foosel/OctoPrint
+iagitup http://github.com/cryptocat/crypt
+iagitup http://github.com/rg3/youtube-dl
+iagitup http://github.com/wifiphisher/wifiphisher
+iagitup http://github.com/Xyl2k/TSA-Travel-Sentry-master-keys 
+iagitup http://github.com/voat/voat
+iagitup http://github.com/majestrate/nntpchan
+iagitup http://github.com/maduce/fosscad-repo
+iagitup http://github.com/nsa-observer/documents
+iagitup http://github.com/offensive-security/cryptsetup-nuke-keys
+iagitup http://github.com/Bitmessage/PyBitmessage
+iagitup http://github.com/jakubroztocil/httpie
+iagitup http://github.com/cjb/GitTorrent
+iagitup http://github.com/int0x80/anti-forensics
+iagitup http://github.com/DanMcInerney/wifijammer
+iagitup http://github.com/humhub/humhub
